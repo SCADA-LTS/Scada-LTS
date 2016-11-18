9e329d590df415196d20b260f7ed6e0486fd39aa
Instalacja <skynurak@gmail.com>
2016-11-18 19:17:06 +0100

    SLTS-59 Improved UI, implemented angular 2 material.

--- ScadaLTS-UI/src/app/app.module.ts
+++ ScadaLTS-UI/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,9 +12,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
