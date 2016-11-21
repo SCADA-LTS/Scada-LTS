44220db5de23b7a6269ff4e575c2151b6f81e2ae
Instalacja <skynurak@gmail.com>
2016-11-21 16:58:50 +0100

    SLTS-59 Improved UI, next version.

--- ScadaLTS-UI/src/app/app.component.ts
+++ ScadaLTS-UI/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-static-content',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
+  isHiddenSubMenu = [false, false, false, false, false];
+
+  private hideElements(){
+    this.isHiddenSubMenu = this.isHiddenSubMenu.map(v => v = false);
+  }
 }
