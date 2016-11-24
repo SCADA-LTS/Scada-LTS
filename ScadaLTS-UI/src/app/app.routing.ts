68926cf1d6eee28f68ece841f7be3c9b231c6560
Instalacja <skynurak@gmail.com>
2016-11-24 19:51:29 +0100

    SLTS-59 Implemented routing to children menu elements + little changes vol.2

--- ScadaLTS-UI/src/app/app.routing.ts
+++ ScadaLTS-UI/src/app/app.routing.ts
@@ -3,11 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppBodyComponent } from './appBody/index';
 import { LoginComponent } from './login/index';
 import { RegistrationComponent } from './registration/index';
+import { WatchlistComponent } from './appBody/watchlist/index';
+import { DashboardComponent } from './appBody/dashboard/index';
 
 const appRoutes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'appBody', component: AppBodyComponent },
   { path: 'registration', component: RegistrationComponent },
+  { path: 'appBody', component: AppBodyComponent,
+    children: [
+      { path: 'watchlist', component: WatchlistComponent },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: '', component: DashboardComponent }
+    ]},
 
   // otherwise redirect to home
   { path: '**', redirectTo: '' }
