13548c7ec8a0254c724e759642fe28f03a2c7870
Instalacja <skynurak@gmail.com>
2016-11-25 20:17:45 +0100

    SLTS-59 Implemented responsive side menu and other stuff.

--- ScadaLTS-UI/src/app/appBody/watchlist/watchlist.component.ts
+++ ScadaLTS-UI/src/app/appBody/watchlist/watchlist.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'watchlist',
   templateUrl: './watchlist.component.html',
   styleUrls: ['./watchlist.component.css']
 })
+
+
+
+
 export class WatchlistComponent {
+
+  items;
+
+  constructor(private http:Http) {
+    this.http.get('/app/appBody/watchlist/items.json')
+      .subscribe(res => this.items = res.json());
+  }
 
 }
