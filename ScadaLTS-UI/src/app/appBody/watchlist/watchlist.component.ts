5f10810658efdc9d197a5757d3404b9e0e4aab5d
Instalacja <skynurak@gmail.com>
2016-12-02 18:28:02 +0100

    SLTS-59 Implemented external library - C3.js - for charts.

--- ScadaLTS-UI/src/app/appBody/watchlist/watchlist.component.ts
+++ ScadaLTS-UI/src/app/appBody/watchlist/watchlist.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
-declare var c3:any;
+import * as d3 from "d3";
+import * as c3 from "c3";
+
 @Component({
   selector: 'watchlist',
   templateUrl: './watchlist.component.html',
   styleUrls: ['./watchlist.component.css']
 })
 
-export class WatchlistComponent {
+export class WatchlistComponent implements OnInit {
 
   items;
 
@@ -23,6 +25,78 @@ export class WatchlistComponent {
       this.watchlist.push(item);
   };
 
+
+  private start() {
+    // let canvas = d3.select("#chart").append("svg").attr("width", 400).attr("height", 400);
+    //
+    // let circle = canvas.append("circle").attr("cx", 10).attr("cy", 10).attr("r", 50).attr("fill", "blue");
+
+    // d3.json("elements.json", function(data){
+    //   let radius = 100;
+    //   let color = d3.scale.ordinal().range(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']);
+    //
+    //   let canvas = d3.select("#chart").append('svg').attr('width', 1000).attr('height', 1000);
+    //
+    //   let group = canvas.append('g').attr('transform', 'translate(500,350)');
+    //
+    //   let arc = d3.svg.arc().innerRadius(150).outerRadius(radius);
+    //
+    //   let pie = d3.layout.pie().value(function(d){
+    //     return d.rank;
+    //   });
+    //
+    //   let theArc = group.selectAll(".arc").data(pie(data)).enter().append("g").attr("class", "arc");
+    //
+    //   // theArc.append("path").attr("d", arc).attr("fill", function(d){
+    //   //   return color(d.data.rank);
+    //   // });
+    //
+    //   theArc.append("text").attr("transform", function(d) {
+    //     return "translate(" + arc.centroid(d) + ")";
+    //   }).attr("dy", "0.15em").text(function(d){
+    //     return d.data.name;
+    //   });
+    // });
+
+    // let canvas = d3.select("#chart").append("svg").attr("width", 500).attr("height", 500);
+    //
+    // let orangeData = [10, 30, 50, 100];
+    //
+    // let oranges = canvas.selectAll("circle").data(orangeData).enter().append("circle").attr("fill", "orange").attr("cx", function(d,i){
+    //   return d + (i * 100);
+    // }).attr("cy", function(d){
+    //   return d;
+    // }).attr("r", function(d){
+    //   return d;
+    // });
+    //
+    // let chart = c3.generate({
+    //   bindto: '#chart',
+    //   data: {
+    //     columns: [
+    //       ['data1', 30, 200, 100, 400, 150, 250],
+    //       ['data2', 50, 20, 10, 40, 15, 25]
+    //     ]
+    //   }
+    // });
+
+    let chart = c3.generate({
+      bindto: '#chart',
+      data: {
+        columns: [
+          ['data1', 30, 200, 100, 400, 150, 250],
+          ['data2', 50, 20, 10, 40, 15, 25]
+        ]
+      }
+    });
+
+  }
+
+
+
+  ngOnInit(){
+    this.start();
+  }
 
   // type = 'line';
   // data = {
