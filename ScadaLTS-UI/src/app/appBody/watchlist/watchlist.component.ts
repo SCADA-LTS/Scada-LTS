f4c50a45aef02ad72f7c3d03940317f5524a8c7d
wisyr <skynurak@gmail.com>
2016-12-29 15:42:38 +0100

    SLTS-96 Update.

--- ScadaLTS-UI/src/app/appBody/watchlist/watchlist.component.ts
+++ ScadaLTS-UI/src/app/appBody/watchlist/watchlist.component.ts
@@ -1,8 +1,8 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {Http} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
-declare var c3: any;
-declare var Plotly:any;
+declare let c3: any;
+declare let Plotly:any;
 
 @Component({
   selector: 'watchlist',
@@ -12,73 +12,83 @@ declare var Plotly:any;
 
 export class WatchlistComponent implements OnInit {
 
-  private _items: Array<WatchlistComponent> = [];
+  private _watchlists: Array<WatchlistComponent> = [];
   private _watchlistElements: Array<WatchlistComponent> = [];
   private _values: Array<WatchlistComponent> = [];
   private xid: string;
   private value: string;
+  private ts: number;
   private name: any;
   private loadPoints;
-
-  constructor(@Inject(Http) private http: Http) {                                                                      //pobiera wszystkie zapisane watchlisty uzytkownika
-    this.http.get('http://localhost:/ScadaBR/api/watchlist/getNames')
-      .subscribe(res => this._items = res.json());
-  };
-
-
-  private updateWatchlistTable(xid) {                                                                     //funkcja wykonywana po kazdej zmianie wartosci w select boxie
-    this._watchlistElements = [];
-    this.http.get('http://localhost/ScadaBR/api/watchlist/getPoints/' + xid)
-      .subscribe(res => {
-        this._watchlistElements = res.json();
-          this.getValues();
-      });
-  };
-
-
-  private chartData = [];                                                                                      //przechowuje dane kazdego punktu z danej watchlisty
+  private chartData = [];
   private bool: boolean = true;
   private chartBool: boolean = true;
+  private dataBoolean: boolean = true;
+  private lastActualization;
+  private selectedWatchlist;
 
-  private getValues() {
-    this._values = [];
-    Observable.forkJoin(
-      this._watchlistElements.map(v => {
-        return this.http.get('http://localhost/ScadaBR/api/points/getValue/' + v.xid)
-          .map(res => res.json());
-      })
-    ).subscribe(res => {
-      this._values = res;
-      if (this.bool) {
-        for (let i = 0; i < this._values.length; i++) {
-          this.chartData.push({x:[], y: [], name: ''});
-        }
-        this.chartData.map((v,i) => v.name = this._values[i].name);
-        this.bool = false;
-      }
-      if (this.chartData[0].x.length < 10) {
-        this.chartData.map((v, i) => v.x.push(new Date()) && v.y.push(this._values[i].value));
-      } else {
-        this.chartData.map(v => v.x.splice(0,1) && v.y.splice(0,1));
-        this.chartData.map((v, i) => v.x.push(new Date()) && v.y.push(this._values[i].value));
-      }
-      if (this.chartBool) {
-        this.redrawChart();
-      }
-    });
+  constructor(@Inject(Http) private http: Http) {
+    this.http.get('http://localhost:/ScadaBR/api/watchlist/getNames')
+        .subscribe(res => this._watchlists = res.json());
+    setTimeout(() => {
+      this.updateWatchlistTable(this._watchlists[0].xid);
+      this.selectedWatchlist = this._watchlists[0];
+      this.initiateInterval();
+    }, 500);
   };
 
+  private updateWatchlistTable(xid) {
+    this._watchlistElements = [];
+    this.http.get('http://localhost/ScadaBR/api/watchlist/getPoints/' + xid)
+        .subscribe(res => {
+          this._watchlistElements = res.json();
+          this.getValues();
+        });
+  };
 
+  private cleanChartBeforeDraw() {
+    this.chartData = [];
+    this.bool = true;
+    Plotly.newPlot('plotly', this.chartData);
+  }
 
-  public initiateChart() {
+  private getValues() {
+    if (this.dataBoolean) {
+      Observable.forkJoin(
+          this._watchlistElements.map(v => {
+            return this.http.get('http://localhost/ScadaBR/api/points/getValue/' + v.xid)
+                .map(res => res.json());
+          })
+      ).subscribe(res => {
+        this._values = res;
+        if (this.bool) {
+          for (let i = 0; i < this._values.length; i++) {
+            this.chartData.push({x: [], y: [], name: ''});
+          }
+          this.chartData.map((v, i) => v.name = this._values[i].name);
+          this.bool = false;
+        }
+        if (this.chartData[0].x.length < 10) {
+          this.chartData.map((v, i) => v.x.push(new Date(this._values[i].ts)) && v.y.push(this._values[i].value));
+        } else {
+          this.chartData.map(v => v.x.splice(0, 1) && v.y.splice(0, 1));
+          this.chartData.map((v, i) => v.x.push(new Date(this._values[i].ts)) && v.y.push(this._values[i].value));
+        }
+        if (this.chartBool) {
+          this.redrawChart();
+        }
+        this.lastActualization = new Date(this._values[0].ts);
+      });
+    }
+  };
+
+  private initiateChart() {
     Plotly.newPlot('plotly', this.chartData);
   }
 
   private redrawChart(){
     Plotly.redraw('plotly', this.chartData);
   }
-
-
 
   private initiateInterval() {
     this.loadPoints = setInterval(() => {
@@ -88,6 +98,10 @@ export class WatchlistComponent implements OnInit {
 
   private pauseChart(){
     this.chartBool = !this.chartBool;
+  }
+
+  private pauseDataDownload(){
+    this.dataBoolean = !this.dataBoolean;
   }
 
   private deactivateInterval() {
