2570aa7221af9aaf84632dc9713737d5ee546e5c
grzesiekb <grzegorz.bylica@gmail.com>
2016-12-30 17:00:29 +0100

    SLTS-100 Add method (isLogged) in API

--- src/org/scada_lts/web/mvc/api/AuthenticationAPI.java
+++ src/org/scada_lts/web/mvc/api/AuthenticationAPI.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.scada_lts.mango.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.serotonin.mango.Common;
-import com.serotonin.mango.db.dao.UserDao;
 import com.serotonin.mango.vo.User;
 
 /**
@@ -25,13 +25,14 @@ import com.serotonin.mango.vo.User;
 public class AuthenticationAPI {
 	
 	private static final Log LOG = LogFactory.getLog(AuthenticationAPI.class);
-
+	
+	private UserService userService = new UserService();
+	
 	@RequestMapping(value = "/api/auth/{username}/{password}", method = RequestMethod.GET)
 	public @ResponseBody String setAuthentication(@PathVariable("username") String username, @PathVariable("password") String password, HttpServletRequest request) {
 		LOG.info("/api/auth/{username}/{password} username:" + username);
 		
-		UserDao userDAO = new UserDao();
-		User user = userDAO.getUser(username);
+		User user = userService.getUser(username);
 		
 		Boolean ok = null;
 
@@ -45,7 +46,7 @@ public class AuthenticationAPI {
 		
 		if (ok == null) {
 			// Update the last login time.
-	        userDAO.recordLogin(user.getId());
+	        userService.recordLogin(user.getId());
 
 	        // Add the user object to the session. This indicates to the rest
 	        // of the application whether the user is logged in or not.
@@ -66,4 +67,37 @@ public class AuthenticationAPI {
 		return json;
 		
 	}
+	
+	@RequestMapping(value = "/api/auth/isLogged/{username}", method = RequestMethod.GET)
+	public @ResponseBody String checkIsLogged(@PathVariable("username") String username, HttpServletRequest request) {
+		
+		LOG.info("/api/auth/isLogged/{username} username:"+username);
+		
+		User user = userService.getUser(username);
+		
+		Boolean ok = null;
+		
+		User userInServer = Common.getUser(request);
+		
+		if ( 
+				(user != null && userInServer != null) &&
+				(user.getUsername().equals(userInServer.getUsername()))
+		) {
+			ok = true;
+		} else {
+			ok = false;
+		}
+		
+		ObjectMapper mapper = new ObjectMapper();
+		String json = null;
+		try {
+			json = mapper.writeValueAsString(ok);
+		} catch (JsonProcessingException e) {
+			LOG.error(e);
+		}
+		return json;
+
+	}
+	
+	
 }
