41e00426ed8a44e335524c1796f3ffb9ac7504a3
wisyr <skynurak@gmail.com>
2016-12-31 16:08:10 +0100

    SLTS-102 Update vol.1.

--- ScadaLTS-UI/src/app/login/login.component.ts
+++ ScadaLTS-UI/src/app/login/login.component.ts
@@ -1,40 +1,44 @@
-import { Component, Inject } from '@angular/core';
-import { Router } from '@angular/router';
-import { Http } from '@angular/http';
+import {Component, Inject} from '@angular/core';
+import {Router} from '@angular/router';
+import {Http} from '@angular/http';
 
 @Component({
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 
 export class LoginComponent {
 
-  private username: string;
-  private password: string;
-  private showModal: boolean = false;
-  private response: string;
+    private username: string;
+    private password: string;
+    private showModal: boolean = false;
+    private response: string;
 
-  private modal(){
-    this.showModal = !this.showModal;
-  }
+    private modal() {
+        this.showModal = !this.showModal;
+    }
 
-  private handle(error: any): Promise<any> {
-    console.error('An error occurred!', error);
-    return Promise.reject(error.message || error);
-  }
+    private handle(error: any): Promise<any> {
+        console.error('An error occurred!', error);
+        return Promise.reject(error.message || error);
+    }
 
-  constructor(@Inject(Http) private http: Http, private router: Router) {};
+    constructor(@Inject(Http) private http: Http, private router: Router) {
+    };
 
-  private login() {
-    this.http.get(`http://localhost/ScadaBR/api/auth/${this.username}/${this.password}`)
-        .map(res => res.json())
-        .catch(this.handle)
-        .subscribe(res => {
-          this.response = res;
-          if (this.response) {
-            this.router.navigate(['/appBody']);
-          }
-        });
-  }
+    private login() {
+        this.http.get(`http://localhost/ScadaBR/api/auth/${this.username}/${this.password}`)
+            .map(res => res.json())
+            .catch(this.handle)
+            .subscribe(res => {
+                this.response = res;
+                if (this.response) {
+                    console.log('access granted');
+                    this.router.navigate(['/appBody']);
+                } else {
+                    console.log('acces denied');
+                }
+            });
+    }
 
 }
