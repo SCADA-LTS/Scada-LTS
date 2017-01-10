498f0ff6b506e4abd52919cd8c82fc750ca1a9dc
grzesiekb <grzegorz.bylica@gmail.com>
2017-01-10 19:22:52 +0100

    SLTS-106 Next correct migration db.

--- src/org/scada_lts/dao/migration/mysql/V1_1__ViewsHierarchy.java
+++ src/org/scada_lts/dao/migration/mysql/V1_1__ViewsHierarchy.java
@@ -1,8 +1,28 @@
+/*
+ * (c) 2016 Abil'I.T. http://abilit.eu/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package org.scada_lts.dao.migration.mysql;
 
 import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+/**
+ * @author grzegorz bylica Abil'I.T. development team, sdt@abilit.eu
+ */
 public class V1_1__ViewsHierarchy implements SpringJdbcMigration {
 	
 	public void migrate(JdbcTemplate jdbcTmp) throws Exception {
@@ -15,22 +35,7 @@ public class V1_1__ViewsHierarchy implements SpringJdbcMigration {
 		    		+ ") engine=innodb;";
 		
 		 jdbcTmp.execute(pointHierarchySQL);
-		 
-		 /*final String funcViewsHierarchyAdd = ""
-			+ "create function `func_views_hierarchy_add`(`a_parentId` int,`a_name` char(100)) "
-				+ "return int "
-				+ "language sql "
-				+ "not deterministic "
-				+ "modifies sql data "
-				+ "sql security definer "
-				+ "comment '' "
-				+ "begin "
-					+ "insert into views_hierarchy (parentId, name) values (a_parentId, a_name); "
-					+ "return last_insert_id(); "
-				+ "end ";
-			 
-		  jdbcTmp.execute(funcViewsHierarchyAdd);*/
-		
+		 		
 	}
 
 }
--- src/org/scada_lts/dao/migration/mysql/V1__BaseVersion.java
+++ src/org/scada_lts/dao/migration/mysql/V1__BaseVersion.java
@@ -1,3 +1,20 @@
+/*
+ * (c) 2016 Abil'I.T. http://abilit.eu/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package org.scada_lts.dao.migration.mysql;
 
 import java.util.LinkedList;
@@ -11,6 +28,9 @@ import com.serotonin.mango.db.dao.UserDao;
 import com.serotonin.mango.vo.User;
 import com.serotonin.mango.vo.permission.DataPointAccess;
 
+/**
+ * @author grzegorz bylica Abil'I.T. development team, sdt@abilit.eu
+ */
 public class V1__BaseVersion implements SpringJdbcMigration {
 	
 	public void migrate(JdbcTemplate jdbcTemplate) throws Exception {
