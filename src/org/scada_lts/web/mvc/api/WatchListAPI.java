7e959e5d29f86f119ca1e3504ff31ef2775e1e0f
grzesiekb <grzegorz.bylica@gmail.com>
2017-01-11 18:16:40 +0100

    SLTS-112 Correct catch exception in REST API.

--- src/org/scada_lts/web/mvc/api/WatchListAPI.java
+++ src/org/scada_lts/web/mvc/api/WatchListAPI.java
@@ -12,13 +12,13 @@ import org.apache.commons.logging.LogFactory;
 import org.scada_lts.mango.service.DataPointService;
 import org.scada_lts.mango.service.PointValueService;
 import org.scada_lts.mango.service.WatchListService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.serotonin.mango.Common;
 import com.serotonin.mango.rt.dataImage.PointValueTime;
@@ -46,53 +46,55 @@ public class WatchListAPI {
 	private DataPointService dataPointService;
 
 	@RequestMapping(value = "/api/watchlist/getNames", method = RequestMethod.GET)
-	public @ResponseBody String getNames(HttpServletRequest request) {
+	public ResponseEntity<String> getNames(HttpServletRequest request) {
 		LOG.info("/api/watchlist/getNames");
+		try {
+			User user = Common.getUser(request);
 		
-		User user = Common.getUser(request);
-		
-		if (user != null) {
-		
-			class WatchListJSON implements Serializable{
-				private String xid;
-				private String name;
-				WatchListJSON(String xid,String name) {
-					this.setXid(xid);
-					this.setName(name);
-				}
-				public String getXid() {
-					return xid;
-				}
-				public void setXid(String xid) {
-					this.xid = xid;
-				}
-				public String getName() {
-					return name;
-				}
-				public void setName(String name) {
-					this.name = name;
-				}
-			}
+			if (user != null) {
 			
-			int userId = user.getId();
-			int profileId = user.getUserProfile();
-			List<WatchList> lstWL = watchListService.getWatchLists(userId, profileId);
-			List<WatchListJSON> lst = new ArrayList<WatchListJSON>();
-			for (WatchList wl:lstWL) {
-				WatchListJSON wlJ = new WatchListJSON(wl.getXid(), wl.getName());
-				lst.add(wlJ);
-			}
-			
-			String json = null;
-			ObjectMapper mapper = new ObjectMapper();
-			try {
+				class WatchListJSON implements Serializable{
+					private String xid;
+					private String name;
+					WatchListJSON(String xid,String name) {
+						this.setXid(xid);
+						this.setName(name);
+					}
+					public String getXid() {
+						return xid;
+					}
+					public void setXid(String xid) {
+						this.xid = xid;
+					}
+					public String getName() {
+						return name;
+					}
+					public void setName(String name) {
+						this.name = name;
+					}
+				}
+				
+				int userId = user.getId();
+				int profileId = user.getUserProfile();
+				List<WatchList> lstWL = watchListService.getWatchLists(userId, profileId);
+				List<WatchListJSON> lst = new ArrayList<WatchListJSON>();
+				for (WatchList wl:lstWL) {
+					WatchListJSON wlJ = new WatchListJSON(wl.getXid(), wl.getName());
+					lst.add(wlJ);
+				}
+				
+				String json = null;
+				ObjectMapper mapper = new ObjectMapper();
 				json = mapper.writeValueAsString(lst);
-			} catch (JsonProcessingException e) {
-				LOG.error(e);
-			}
-			return json;
-		} else {
-			return null;
+				
+				return new ResponseEntity<String>(json,HttpStatus.OK);				
+			} 
+			
+			return new ResponseEntity<String>(HttpStatus.UNAUTHORIZED);
+			
+		} catch (Exception e) {
+			LOG.error(e);
+			return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
 		}
 	}
 	
@@ -103,123 +105,129 @@ public class WatchListAPI {
 	 * @return
 	 */
 	@RequestMapping(value = "/api/watchlist/getPoints/{xid}", method = RequestMethod.GET)
-	public @ResponseBody String getPoints(@PathVariable("xid") String xid, HttpServletRequest request) {
+	public ResponseEntity<String> getPoints(@PathVariable("xid") String xid, HttpServletRequest request) {
 		
 		LOG.info("/api/watchlist/getPoints/{xid} xid:"+xid);
 		
-		// check may use watch list
-		User user = Common.getUser(request);
+		try {
+			User user = Common.getUser(request);
 		
-		if (user != null) {
-			class PointJSON implements Serializable{
-				private String xid;
-				private String name;
-				PointJSON(String xid,String name) {
-					this.setXid(xid);
-					this.setName(name);
+			if (user != null) {
+				class PointJSON implements Serializable{
+					private String xid;
+					private String name;
+					PointJSON(String xid,String name) {
+						this.setXid(xid);
+						this.setName(name);
+					}
+					public String getXid() {
+						return xid;
+					}
+					public void setXid(String xid) {
+						this.xid = xid;
+					}
+					public String getName() {
+						return name;
+					}
+					public void setName(String name) {
+						this.name = name;
+					}
 				}
-				public String getXid() {
-					return xid;
-				}
-				public void setXid(String xid) {
-					this.xid = xid;
-				}
-				public String getName() {
-					return name;
-				}
-				public void setName(String name) {
-					this.name = name;
-				}
-			}
 			
-			WatchList wl = watchListService.getWatchList(xid);
-			watchListService.populateWatchlistData(wl);
-			List<PointJSON> lst = new ArrayList<PointJSON>();
+				WatchList wl = watchListService.getWatchList(xid);
+				watchListService.populateWatchlistData(wl);
+				List<PointJSON> lst = new ArrayList<PointJSON>();
 			
-			for (DataPointVO dpvo : wl.getPointList()){
-				PointJSON p = new PointJSON(dpvo.getXid(), dpvo.getName());
-				lst.add(p);
-			}
+				for (DataPointVO dpvo : wl.getPointList()){
+					PointJSON p = new PointJSON(dpvo.getXid(), dpvo.getName());
+					lst.add(p);
+				}
+				
+				String json = null;
+				ObjectMapper mapper = new ObjectMapper();
 			
-			String json = null;
-			ObjectMapper mapper = new ObjectMapper();
-			
-			try {
 				json = mapper.writeValueAsString(lst);
-			} catch (JsonProcessingException e) {
-				LOG.error(e);
+				
+			
+				return new ResponseEntity<String>(json,HttpStatus.OK);
 			}
 			
-			return json;
-		} else {
-			return null;
+			return new ResponseEntity<String>(HttpStatus.UNAUTHORIZED);
+			
+		} catch (Exception e) {
+			LOG.error(e);
+			return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
 		}
 	}
 	
 	@RequestMapping(value = "/api/watchlist/getChartData/{xid}/{fromData}/{toData}", method = RequestMethod.GET)
-	public @ResponseBody String getChartData(@PathVariable("xid") String xid, @PathVariable("fromData") Long fromData, @PathVariable("toData") Long toData, HttpServletRequest request) {
+	public ResponseEntity<String> getChartData(@PathVariable("xid") String xid, @PathVariable("fromData") Long fromData, @PathVariable("toData") Long toData, HttpServletRequest request) {
 		
 		LOG.info("/api/watchlist/getChartData/{xid}/{fromData}/{toData} xid:"+xid+" fromData:"+fromData+" toData:"+toData);
 		
-		User user = Common.getUser(request);
+		try {
+			User user = Common.getUser(request);
 		
-		if (user != null) {
-			
-			class DataChartJSON implements Serializable{
-				private String xid;
-				private String name;
-				private List<ValueToJSON> values;
-				DataChartJSON(String xid, String name, List<ValueToJSON> values) {
-					this.setXid(xid);
-					this.setName(name);
-					this.setValues(values);
+			if (user != null) {
+				class DataChartJSON implements Serializable{
+					private String xid;
+					private String name;
+					private List<ValueToJSON> values;
+					DataChartJSON(String xid, String name, List<ValueToJSON> values) {
+						this.setXid(xid);
+						this.setName(name);
+						this.setValues(values);
+					}
+					public String getXid() {
+						return xid;
+					}
+					public void setXid(String xid) {
+						this.xid = xid;
+					}
+					public String getName() {
+						return name;
+					}
+					public void setName(String name) {
+						this.name = name;
+					}
+					public List<ValueToJSON> getValues() {
+						return values;
+					}
+					public void setValues(List<ValueToJSON> values) {
+						this.values=values;
+					}
 				}
-				public String getXid() {
-					return xid;
+			
+				DataPointVO dp = dataPointService.getDataPoint(xid);
+				
+				List<PointValueTime> listValues = pointValueService.getPointValuesBetween(dp.getId(), fromData, toData);
+				
+				List<ValueToJSON> values = new ArrayList<ValueToJSON>();
+				
+				for (PointValueTime pvt : listValues) {
+					ValueToJSON v = new ValueToJSON();
+					v.set(pvt, dp);
+					values.add(v);
 				}
-				public void setXid(String xid) {
-					this.xid = xid;
-				}
-				public String getName() {
-					return name;
-				}
-				public void setName(String name) {
-					this.name = name;
-				}
-				public List<ValueToJSON> getValues() {
-					return values;
-				}
-				public void setValues(List<ValueToJSON> values) {
-					this.values=values;
-				}
-			}
 			
-			DataPointVO dp = dataPointService.getDataPoint(xid);
-			
-			List<PointValueTime> listValues = pointValueService.getPointValuesBetween(dp.getId(), fromData, toData);
-			
-			List<ValueToJSON> values = new ArrayList<ValueToJSON>();
-			
-			for (PointValueTime pvt : listValues) {
-				ValueToJSON v = new ValueToJSON();
-				v.set(pvt, dp);
-				values.add(v);
-			}
-			
-			DataChartJSON dataChartJSON = new DataChartJSON(xid, dp.getName(), values);
-			
-			String json = null;
-			ObjectMapper mapper = new ObjectMapper();
-			
-			try {
+				DataChartJSON dataChartJSON = new DataChartJSON(xid, dp.getName(), values);
+				
+				String json = null;
+				ObjectMapper mapper = new ObjectMapper();
+				
 				json = mapper.writeValueAsString(dataChartJSON);
-			} catch (JsonProcessingException e) {
-				LOG.error(e);
+				
+				return new ResponseEntity<String>(json,HttpStatus.OK);
 			}
-			return json;
-		} else {
-			return "";
+			
+			return new ResponseEntity<String>(HttpStatus.UNAUTHORIZED);
+		
+		} catch (Exception e) {
+			LOG.error(e);
+			return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
 		}
+		
+		
 	}
 
 }
