07e5758e0df227170d4f6bb5c44d9a1c1082dc80
Radek Jajko <radek235s@gmail.com>
2017-08-08 13:02:31 +0200

    #332 Simulation: Added - saving values to ScadaDataPoint

--- ScadaLTS-Simulation1/war/WebContent/config.js
+++ ScadaLTS-Simulation1/war/WebContent/config.js
@@ -4,6 +4,7 @@ var viewConfiguration = {
     cardDescription: ["All day: ", "Enable/Disable", "Current Hour", "Last Hour", "Current Day"],
     logoSrc: "resources/logo.png",
     enableButtonDSxid: "DP_558690",
+    configLocationDSxid: "DP_558691",
     /* weekEnableShedule [mon, tue, wed, th, fri, sat, sun] :: 0 is disable 1 is enable */
     weekEnableShedule: [0, 1, 1, 1, 1, 0, 0],
     /* weekEnableHours [minHour, maxHour] :: when time is beatween this values DS is enabled */
