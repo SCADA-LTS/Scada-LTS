1658df35fb7aaf69e47e26aa8c865afb1f1766d6
Arkadiusz Parafiniuk <arkadiusz.parafiniuk@gmail.com>
2018-01-12 13:56:04 +0100

    #444 Added docker configuration for user management module url

--- docker/start.sh
+++ docker/start.sh
@@ -5,6 +5,7 @@ sleep 10
 mysql -uroot -proot -e "create database if not exists scadalts"
 cd /opt/scadalts
 ./bin/startup.sh
+sleep 120
 cd
 cd /opt/scadalts/webapps/ScadaBR/WEB-INF/classes
 sed -i 's/localhost:8090/'`echo $DOCKER_HOST_IP`:8090'/g' env.properties
