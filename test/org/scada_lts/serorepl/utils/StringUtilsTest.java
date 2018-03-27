c7aaf4ae6a6e58041213842d35f8ae26c0027ce0
Grzegorz Łysyganicz <glysyganicz@softq.pl>
2018-03-27 14:33:58 +0200

    Test refactroing

--- test/org/scada_lts/serorepl/utils/StringUtilsTest.java
+++ test/org/scada_lts/serorepl/utils/StringUtilsTest.java
@@ -2,11 +2,7 @@ package org.scada_lts.serorepl.utils;
 
 import com.serotonin.util.StringUtils;
 import org.junit.Test;
-
-import java.util.regex.PatternSyntaxException;
-
 import static org.junit.Assert.*;
-
 public class StringUtilsTest {
 
     @Test
@@ -32,7 +28,6 @@ public class StringUtilsTest {
     public void escapeLT() throws Exception {
         String string1 = " <<<<  << >> <>< jJjKlju UJJ8kmi  ikkiu IK i,koi ";
         String string2 = "<<<<<<<<>";
-        String string3 = "";
         assertEquals(StringUtils.escapeLT(string1),
                 org.scada_lts.serorepl.utils.StringUtils.escapeLT(string1));
 
@@ -49,12 +44,10 @@ public class StringUtilsTest {
                 org.scada_lts.serorepl.utils.StringUtils.escapeLT(null));
     }
 
-
     @Test
     public void generateRandomString() throws Exception {
 
         String charSet1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-
         assertFalse(org.scada_lts.serorepl.utils.StringUtils.generateRandomString(15, charSet1).contains("!"));
         assertFalse(org.scada_lts.serorepl.utils.StringUtils.generateRandomString(15, charSet1).contains("@"));
         assertFalse(org.scada_lts.serorepl.utils.StringUtils.generateRandomString(15, charSet1).contains("#"));
@@ -64,15 +57,21 @@ public class StringUtilsTest {
         assertFalse(org.scada_lts.serorepl.utils.StringUtils.generateRandomString(15, charSet1).contains("<"));
         assertTrue(org.scada_lts.serorepl.utils.StringUtils.generateRandomString(15, charSet1).length() == 15);
         assertFalse(org.scada_lts.serorepl.utils.StringUtils.generateRandomString(15, charSet1).length() != 15);
-
     }
-
 
     @Test
     public void globWhiteListMatchIgnoreCase() throws Exception {
-        // TODO
-    }
 
+        String [] values1 = new String[]{"gsgsg*", "ssss"};
+        String value_s1 = "gsgsg";
+        String value_s2 = "gsAsd  32 gsg";
+
+        assertTrue(org.scada_lts.serorepl.utils.StringUtils.globWhiteListMatchIgnoreCase(values1,value_s1));
+        assertFalse(org.scada_lts.serorepl.utils.StringUtils.globWhiteListMatchIgnoreCase(values1,value_s2));
+
+        assertEquals(StringUtils.globWhiteListMatchIgnoreCase(values1,value_s1) ,
+                org.scada_lts.serorepl.utils.StringUtils.globWhiteListMatchIgnoreCase(values1,value_s1));
+    }
 
     @Test
     public void isBetweenInc() throws Exception {
@@ -92,7 +91,6 @@ public class StringUtilsTest {
         assertTrue(org.scada_lts.serorepl.utils.StringUtils.isEmpty(emptyArray));
     }
 
-
     @Test
     public void isEmptyString() throws Exception {
         String s1 = "String";
@@ -100,7 +98,6 @@ public class StringUtilsTest {
         assertFalse(org.scada_lts.serorepl.utils.StringUtils.isEmpty(s1));
         assertTrue(org.scada_lts.serorepl.utils.StringUtils.isEmpty(s2));
     }
-
 
     @Test
     public void isEqual() throws Exception {
@@ -131,7 +128,6 @@ public class StringUtilsTest {
 
         assertEquals(StringUtils.pad("RANDOM", 'X', 7)  ,
         org.scada_lts.serorepl.utils.StringUtils.pad("RANDOM", 'X', 7));
-
     }
 
     @Test
@@ -143,11 +139,8 @@ public class StringUtilsTest {
         assertEquals(org.scada_lts.serorepl.utils.StringUtils.parseInt("siemanko", 99), 99);
     }
 
-
-
     @Test
     public void trimWhitespace() throws Exception {
-
         String s1 = "RANDOM";
         String s2 = "ASDASDASD ASDASD QWEQWE ZXCZXC";
         String s3 = "    asdxc  qwe  qwe qw " +
@@ -175,20 +168,10 @@ public class StringUtilsTest {
                 org.scada_lts.serorepl.utils.StringUtils.trimWhitespace(s6));
     }
 
-
     @Test
     public void replaceMacro() throws Exception {
         assertEquals( org.scada_lts.serorepl.utils.StringUtils.replaceMacro("randomowy string z jakims ${parametr}","parametr","PARAM"), "randomowy string z jakims PARAM" );
-
     }
-
-    @Test//(expected = PatternSyntaxException.class)
-    public void replaceMacroFailAtWrongArguments() throws Exception{
-            // TODO sprawdzic ze poleci exception jak jest zly 2 argument (nie znajdzie w stringu s patternu)
-          //  org.scada_lts.serorepl.utils.StringUtils.replaceMacro("randomowy string z jakims ${parametr}","INVALID_PARAM","PARAM");
-
-    }
-
 
     @Test
     public void replaceMacros() throws Exception {
@@ -207,12 +190,6 @@ public class StringUtilsTest {
         System.out.println(org.scada_lts.serorepl.utils.StringUtils.replaceMacros("  d    ", System.getProperties()));
         System.out.println(org.scada_lts.serorepl.utils.StringUtils.replaceMacros(" SD Sdasd ", System.getProperties()));
         System.out.println(org.scada_lts.serorepl.utils.StringUtils.replaceMacros(" si            emano \n kolano mariano \nitaliano enter to byl \nczy nie enter? ", System.getProperties()));
-
-    }
-
-    @Test
-    public void truncate1Parameter() throws Exception {
-
     }
 
     @Test
@@ -240,12 +217,6 @@ public class StringUtilsTest {
     public void isEqualIgnoreCase() throws Exception {
     }
 
-
-
-
-
-
-
     @Test
     public void isEmpty2() throws Exception {
     }
@@ -257,8 +228,6 @@ public class StringUtilsTest {
     @Test
     public void isLengthLessThan() throws Exception {
     }
-
-
 
     @Test
     public void containsUppercase() throws Exception {
@@ -272,7 +241,6 @@ public class StringUtilsTest {
     public void containsDigit() throws Exception {
     }
 
-
     @Test
     public void mask() throws Exception {
     }
@@ -285,7 +253,6 @@ public class StringUtilsTest {
     public void generatePassword1() throws Exception {
     }
 
-
     @Test
     public void isOneOf() throws Exception {
     }
@@ -294,10 +261,6 @@ public class StringUtilsTest {
     public void isOneOf1() throws Exception {
     }
 
-
-
-
-
     @Test
     public void replaceMacro1() throws Exception {
     }
@@ -305,8 +268,6 @@ public class StringUtilsTest {
     @Test
     public void getMacroContent() throws Exception {
     }
-
-
 
     @Test
     public void findGroup() throws Exception {
@@ -328,12 +289,9 @@ public class StringUtilsTest {
     public void in() throws Exception {
     }
 
-
     @Test
     public void durationToString() throws Exception {
     }
-
-
 
     @Test
     public void startsWith() throws Exception {
