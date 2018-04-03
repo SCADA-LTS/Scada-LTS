ae3761d7d7ad9945be3cde5aa2a388f7602a1da1
Grzegorz Łysyganicz <glysyganicz@softq.pl>
2018-04-03 14:53:11 +0200

    toString, hashCode and equals methods added

--- src/org/scada_lts/serorepl/utils/DirectoryInfo.java
+++ src/org/scada_lts/serorepl/utils/DirectoryInfo.java
@@ -8,7 +8,31 @@ public class DirectoryInfo implements Serializable{
     private int count;
     private long size;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        DirectoryInfo that = (DirectoryInfo) o;
+
+        if (count != that.count) return false;
+        return size == that.size;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = count;
+        result = 31 * result + (int) (size ^ (size >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "DirectoryInfo{" +
+                "count=" + count +
+                ", size=" + size +
+                '}';
+    }
 
     public DirectoryInfo() {
     }
