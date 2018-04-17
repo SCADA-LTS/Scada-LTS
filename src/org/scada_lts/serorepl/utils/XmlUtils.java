7b9cf1afd62b393132e92441df5c5d5e6540710b
Grzegorz Łysyganicz <glysyganicz@softq.pl>
2018-04-17 14:30:11 +0200

    Added serorpl classes Test not working now

---
+++ src/org/scada_lts/serorepl/utils/XmlUtils.java
@@ -0,0 +1,42 @@
+package org.scada_lts.serorepl.utils;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class XmlUtils {
+
+    private final DocumentBuilder builder;
+    private TransformerFactory transformerFactory;
+
+    public XmlUtils() throws ParserConfigurationException {
+        builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+    }
+
+    public Document parse(File inputFile) throws IOException, SAXException {
+        return builder.parse(inputFile);
+    }
+
+
+    public List<Element> getElementsByTagName(Element parentElement, String tagName){
+        NodeList nodeList = parentElement.getElementsByTagName(tagName);
+        ArrayList<Element> results = new ArrayList<>();
+
+        for (int index = 0 ; index < nodeList.getLength(); index++){
+            results.add((Element)nodeList.item(index));
+        }
+        return results;
+    }
+
+}
