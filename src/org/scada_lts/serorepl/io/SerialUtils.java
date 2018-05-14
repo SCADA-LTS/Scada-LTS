4e5958af2478ad688ae8c311282266c4aa67260e
Grzegorz Łysyganicz <glysyganicz@softq.pl>
2018-05-14 10:54:46 +0200

    Added serialutils class

--- src/org/scada_lts/serorepl/io/SerialUtils.java
+++ src/org/scada_lts/serorepl/io/SerialUtils.java
@@ -1,35 +1,40 @@
 package org.scada_lts.serorepl.io;
 
-//import jssc.SerialPort;
-//import jssc.SerialPortException;
-//import javax.comm.SerialPort;
-//import javax.comm.CommPortIdentifier;
-//import javax.comm.NoSuchPortException;
-//import javax.comm.PortInUseException;
+import jssc.SerialPortException;
+
+import javax.comm.*;
+
+import static jssc.SerialPortException.TYPE_INCORRECT_SERIAL_PORT;
 
 public class SerialUtils {
 
-   /* public static SerialPort openSerialPort(StreamProperties serialParameters) throws SerialPortException {
+    public static javax.comm.SerialPort openSerialPort(StreamProperties serialParameters) throws SerialPortException {
         SerialPort serialPort = null;
 
         try {
             CommPortIdentifier commPortIdentifier = CommPortIdentifier.getPortIdentifier(serialParameters.getCommPortId());
 
             if(commPortIdentifier.getPortType() != 1){
-               // throw new SerialPortException("Port with ID: " + serialParameters.getCommPortId() + " is not a serial port");
+                throw new SerialPortException( serialParameters.getCommPortId(),"openSerialPort", TYPE_INCORRECT_SERIAL_PORT );
             }else {
                 serialPort = (SerialPort) commPortIdentifier.open(serialParameters.getPortOwnerName(), 1000);
+                serialPort.setSerialPortParams(serialParameters.getBaudRate(), serialParameters.getDataBits(), serialParameters.getStopBits(), serialParameters.getParity());
+                serialPort.setFlowControlMode(serialParameters.getFlowControlIn() | serialParameters.getFlowControlOut());
+                return serialPort;
             }
 
-        } catch (NoSuchPortException e) {
-            e.printStackTrace();
-        } catch (PortInUseException e) {
+        } catch (NoSuchPortException | PortInUseException | UnsupportedCommOperationException e) {
+            close(serialPort);
             e.printStackTrace();
         }
+        return serialPort;
     }
 
     public static void close(SerialPort serialPort) {
+        if (serialPort != null){
 
-    }*/
+            serialPort.close();
+        }
+    }
 
 }
