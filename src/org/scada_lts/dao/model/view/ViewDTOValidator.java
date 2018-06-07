457026201607335e1cc7876a4c7fedd848ca3b8c
Grzegorz Łysyganicz <glysyganicz@softq.pl>
2018-06-07 13:23:12 +0200

    Refactoring

--- src/org/scada_lts/dao/model/view/ViewDTOValidator.java
+++ src/org/scada_lts/dao/model/view/ViewDTOValidator.java
@@ -5,6 +5,13 @@ import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 public class ViewDTOValidator implements Validator {
+
+    private static final int VIEW_SIEZE_MIN = 0;
+    private static final int VIEW_SIEZE_MAX = 4;
+    private static final int NAME_MAX_SIZE = 100;
+    private static final int XID_MAX_SIZE = 50;
+    private static final int BACKGROUND_PATH_MAX_SIZE = 255;
+
     @Override
     public boolean supports(Class<?> aClass) {
         return ViewDTO.class.equals(aClass);
@@ -18,14 +25,17 @@ public class ViewDTOValidator implements Validator {
         ValidationUtils.rejectIfEmpty(errors, "xid", "xid.empty");
         ValidationUtils.rejectIfEmpty(errors, "imagePath", "imagePath.empty");
 
-        if (view.getSize() < 0 || view.getSize() > 4){
+        if (view.getSize() < VIEW_SIEZE_MIN || view.getSize() > VIEW_SIEZE_MAX) {
             errors.rejectValue("size", "Values out of range");
         }
-        if (view.getName().length() > 100){
+        if (view.getName().length() > NAME_MAX_SIZE) {
             errors.rejectValue("name", "Name is too large");
         }
-
-
-
+        if (view.getXid().length() > XID_MAX_SIZE) {
+            errors.rejectValue("xid", "Xid is too large");
+        }
+        if (view.getImagePath().length() > BACKGROUND_PATH_MAX_SIZE) {
+            errors.rejectValue("background", "Background path is too large");
+        }
     }
 }
