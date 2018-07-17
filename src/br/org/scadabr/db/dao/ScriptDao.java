876f561200629ad26a36fde3e1a00f7d14aea97a
grzesiekb <grzegorz.bylica@gmail.com>
2018-07-17 16:53:04 +0200

    #630 fix save and update Script

--- src/br/org/scadabr/db/dao/ScriptDao.java
+++ src/br/org/scadabr/db/dao/ScriptDao.java
@@ -5,6 +5,7 @@ import com.serotonin.mango.Common;
 import com.serotonin.mango.db.dao.BaseDao;
 import com.serotonin.util.SerializationHelper;
 import org.scada_lts.dao.DAO;
+import org.scada_lts.dao.ScriptDAO;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.transaction.TransactionStatus;
@@ -18,52 +19,56 @@ import java.util.logging.Logger;
 public class ScriptDao extends BaseDao {
 	private static final String SCRIPT_SELECT = "select id, xid, name, script, userId, data from scripts ";
 
+	private ScriptDAO scriptDAO = new ScriptDAO();
+
 	public void saveScript(final ScriptVO<?> vo) {
 		// Decide whether to insert or update.
 		if (vo.getId() == Common.NEW_ID)
-			insertScript(vo);
+			scriptDAO.insert(vo);
+			//insertScript(vo);
 		else
-			updateScript(vo);
+			//updateScript(vo);
+			scriptDAO.update(vo);
 	}
 
 	private void insertScript(final ScriptVO<?> vo) {
-                if (Common.getEnvironmentProfile().getString("db.type").equals("postgres")){                
-                    try {
-                        Connection conn = DriverManager.getConnection(Common.getEnvironmentProfile().getString("db.url"),
-                                                    Common.getEnvironmentProfile().getString("db.username"),
-                                                    Common.getEnvironmentProfile().getString("db.password"));
-                        PreparedStatement preStmt = conn.prepareStatement("insert into scripts (xid, name,  script, userId, data) values (?,?,?,?,?)");
-                        preStmt.setString(1, vo.getXid());
-                        preStmt.setString(2, vo.getName());
-                        preStmt.setString(3, vo.getScript());
-                        preStmt.setInt(4, vo.getUserId());
-                        preStmt.setBytes(5, SerializationHelper.writeObjectToArray(vo));
-                        preStmt.executeUpdate();
-                        
-                        ResultSet resSEQ = conn.createStatement().executeQuery("SELECT currval('scripts_id_seq')");
-                        resSEQ.next();
-                        int id = resSEQ.getInt(1);
+		if (Common.getEnvironmentProfile().getString("db.type").equals("postgres")){
+			try {
+				Connection conn = DriverManager.getConnection(Common.getEnvironmentProfile().getString("db.url"),
+						Common.getEnvironmentProfile().getString("db.username"),
+						Common.getEnvironmentProfile().getString("db.password"));
+				PreparedStatement preStmt = conn.prepareStatement("insert into scripts (xid, name,  script, userId, data) values (?,?,?,?,?)");
+				preStmt.setString(1, vo.getXid());
+				preStmt.setString(2, vo.getName());
+				preStmt.setString(3, vo.getScript());
+				preStmt.setInt(4, vo.getUserId());
+				preStmt.setBytes(5, SerializationHelper.writeObjectToArray(vo));
+				preStmt.executeUpdate();
 
-                        conn.close(); 
-                        
-                        vo.setId(id);
-                        
-                    } catch (SQLException ex) {
-                        Logger.getLogger(FlexProjectDao.class.getName()).log(Level.SEVERE, null, ex);
-                        vo.setId(0);
-                    }
-                }
-                else{
+				ResultSet resSEQ = conn.createStatement().executeQuery("SELECT currval('scripts_id_seq')");
+				resSEQ.next();
+				int id = resSEQ.getInt(1);
 
-					DAO.getInstance().getJdbcTemp().update("insert into scripts (xid, name,  script, userId, data) values (?,?,?,?,?)", new Object[] { vo.getXid(), vo.getName(),
-									vo.getScript(), vo.getUserId(),
-									SerializationHelper.writeObject(vo) },
-							new int[] { Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.INTEGER,
-									Common.getEnvironmentProfile().getString("db.type").equals("postgres") ? Types.BINARY: Types.BLOB } );
+				conn.close();
 
-					vo.setId(DAO.getInstance().getId());
+				vo.setId(id);
 
-                }
+			} catch (SQLException ex) {
+				Logger.getLogger(FlexProjectDao.class.getName()).log(Level.SEVERE, null, ex);
+				vo.setId(0);
+			}
+		}
+		else{
+
+			DAO.getInstance().getJdbcTemp().update("insert into scripts (xid, name,  script, userId, data) values (?,?,?,?,?)", new Object[] { vo.getXid(), vo.getName(),
+							vo.getScript(), vo.getUserId(),
+							SerializationHelper.writeObject(vo) },
+					new int[] { Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.INTEGER,
+							Common.getEnvironmentProfile().getString("db.type").equals("postgres") ? Types.BINARY: Types.BLOB } );
+
+			vo.setId(DAO.getInstance().getId());
+
+		}
 	}
 
 	@SuppressWarnings("unchecked")
@@ -97,16 +102,18 @@ public class ScriptDao extends BaseDao {
 	}
 
 	public ScriptVO<?> getScript(int id) {
-		return (ScriptVO<?>)DAO.getInstance().getJdbcTemp().queryForObject(SCRIPT_SELECT + " where id=?",new Object[] { id }, new ScriptRowMapper() );
+		return scriptDAO.getScript(id);
+		//return (ScriptVO<?>)DAO.getInstance().getJdbcTemp().queryForObject(SCRIPT_SELECT + " where id=?",new Object[] { id }, new ScriptRowMapper() );
 	}
 
 	public List<ScriptVO<?>> getScripts() {
+		return scriptDAO.getScripts();
 
-		List<ScriptVO<?>> scripts = DAO.getInstance().getJdbcTemp().query(SCRIPT_SELECT, new ScriptRowMapper()); //query(SCRIPT_SELECT, new ScriptRowMapper());
-		return scripts;
+		//List<ScriptVO<?>> scripts = DAO.getInstance().getJdbcTemp().query(SCRIPT_SELECT, new ScriptRowMapper()); //query(SCRIPT_SELECT, new ScriptRowMapper());
+		//return scripts;
 	}
 
-	class ScriptRowMapper implements RowMapper<ScriptVO<?>> {
+	/*class ScriptRowMapper implements RowMapper<ScriptVO<?>> {
 		public ScriptVO<?> mapRow(ResultSet rs, int rowNum) throws SQLException {
                         ScriptVO<?> script;
                         if (Common.getEnvironmentProfile().getString("db.type").equals("postgres")){
@@ -114,7 +121,7 @@ public class ScriptDao extends BaseDao {
                         }
                         else{
                             script = (ScriptVO<?>) SerializationHelper.readObject(rs.getBlob(6).getBinaryStream());
-                        }                    
+                        }
 			script.setId(rs.getInt(1));
 			script.setXid(rs.getString(2));
 			script.setName(rs.getString(3));
@@ -122,7 +129,7 @@ public class ScriptDao extends BaseDao {
 			script.setUserId(rs.getInt(5));
 			return script;
 		}
-	}
+	}*/
 
 
 	public boolean isXidUnique(String xid, int excludeId) {
@@ -130,13 +137,14 @@ public class ScriptDao extends BaseDao {
 	}
 
 	public ScriptVO<?> getScript(String xid) {
-		try {
+		return scriptDAO.getScript(xid);
+		/*try {
 			return (ScriptVO<?>)DAO.getInstance().getJdbcTemp().query(SCRIPT_SELECT + " where xid=?",
 					new Object[] { xid }, new ScriptRowMapper());
 
 		} catch (EmptyResultDataAccessException e) {
 			return null;
-		}
+		}*/
 	}
 
 }
