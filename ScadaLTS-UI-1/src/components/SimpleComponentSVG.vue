fc6d7a0a560c67e9a1d064fc47bbdb38f01e0cc1
grzesiekb <grzegorz.bylica@gmail.com>
2018-08-08 15:32:09 +0200

    #663 I added a new SVG component
    We add new components written in vuejs to ultimately go on the UI
    side to the framework vuejs communicating with the backend after
    REST API or notified by websockets.
    
    I modified SimpleComponentSVG so that call REST API for data
    every set time

--- ScadaLTS-UI-1/src/components/SimpleComponentSVG.vue
+++ ScadaLTS-UI-1/src/components/SimpleComponentSVG.vue
@@ -5,41 +5,38 @@
       <rect width="100" height="20" style="fill:rgb(255,255,255);stroke-width:2;stroke:rgb(0,0,0)"/>
       <text x="5" y="16" fill="red">{{value}}</text>
     </svg>
-    <button @click="getData(this.xidPoint)">getData</button>
   </div>
 </template>
 <script>
   import axios from 'axios';
-
   export default {
-    props: ['xidPoint', 'timeRefresh', 'label', 'value'],
+    props: ['pxidPoint', 'ptimeRefresh', 'plabel', 'pvalue'],
     data() {
-      return {}
+      return {
+        xidPoint: this.pxidPoint,
+        timeRefresh: this.ptimeRefresh,
+        label: this.plabel,
+        value: this.pvalue,
+        count:0
+      }
     },
     methods: {
-      getData(xid) {
-        const apiGetValuePoint = `./api/point_value/getValue/${xid}`;
+      getData() {
+        const apiGetValuePoint = `./api/point_value/getValue/${this.xidPoint}`;
         axios.get(apiGetValuePoint).then(response => {
           this.value = response.data.value;
+          this.count ++;
         }).catch(error => {
           console.log(error);
         });
       }
     },
     created() {
-      console.log('prev:'+this.xidPoint);
-      console.log('prev:'+this.timeRefresh);
-      console.log('prev:'+this.label);
-      console.log('prev:'+this.value);
-      this.getData(this.xidPoint);
-      setInterval(
-        this.getData(this.xidPoint),
-        1000
-      );
-      console.log('next:'+this.xidPoint);
-      console.log('next:'+this.timeRefresh);
-      console.log('next:'+this.label);
-      console.log('next:'+this.value);
+      if (this.timeRefresh != undefined) {
+        setInterval(function () {
+          this.getData();
+        }.bind(this), this.timeRefresh);
+      }
     }
   }
 </script>
