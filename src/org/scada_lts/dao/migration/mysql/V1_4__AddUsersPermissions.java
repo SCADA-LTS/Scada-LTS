b0dd112228c8fa115674c22c066b1afa9e106398
Arkadiusz Parafiniuk <arkadiusz.parafiniuk@gmail.com>
2018-09-17 14:19:18 +0200

    #703 Added create usersPermission table to flyway migration

---
+++ src/org/scada_lts/dao/migration/mysql/V1_4__AddUsersPermissions.java
@@ -0,0 +1,24 @@
+package org.scada_lts.dao.migration.mysql;
+
+import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * Class created by Arkadiusz Parafiniuk
+ *
+ * @Author arkadiusz.parafiniuk@gmail.com
+ */
+public class V1_4__AddUsersPermissions implements SpringJdbcMigration {
+
+    @Override
+    public void migrate(JdbcTemplate jdbcTmp) throws Exception {
+        final String usersPermissionsSQL = ""
+                + "CREATE table usersPermissions (  "
+                + "id int not null,  "
+                + "entityXid varchar(50) not null,  "
+                + "permission int not null"
+                + ") ENGINE=InnoDB;";
+
+        jdbcTmp.execute(usersPermissionsSQL);
+    }
+}
