5e1ca3e43d84ff52e969844b3ed61c1b406ba3e7
Arkadiusz Parafiniuk <arkadiusz.parafiniuk@gmail.com>
2018-09-20 12:18:18 +0200

    #703  Extended UserPermissionDAO

--- src/org/scada_lts/usersPermissions/dao/UserPermissionDAO.java
+++ src/org/scada_lts/usersPermissions/dao/UserPermissionDAO.java
@@ -1,9 +1,17 @@
 package org.scada_lts.usersPermissions.dao;
 
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.scada_lts.dao.DataPointDAO;
+import org.scada_lts.dao.DAO;
+import org.scada_lts.usersPermissions.model.UserPermission;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Class created by Arkadiusz Parafiniuk
@@ -14,5 +22,57 @@ import org.springframework.stereotype.Repository;
 public class UserPermissionDAO {
 
     private static final Log LOG = LogFactory.getLog(UserPermissionDAO.class);
+
+    private static final String TABLE_NAME = "usersPermissions";
+    private static final String COLUMN_NAME_ID = "id";
+    private static final String COLUMN_NAME_ENTITY_XID = "entityXid";
+    private static final String COLUMN_NAME_PERMISSION = "permission";
+
+    // @formatter:off
+
+    private static final String USER_PERMISSION_SELECT_ALL = ""
+            + "select "
+            + COLUMN_NAME_ID + ", "
+            + COLUMN_NAME_ENTITY_XID + ", "
+            + COLUMN_NAME_PERMISSION + " "
+            + "from "
+            + TABLE_NAME + ";";
+
+    private static final String USER_PERMISSION_SELECT_WHERE_ENTITY_XID = ""
+            + "select "
+            + COLUMN_NAME_ID + ", "
+            + COLUMN_NAME_ENTITY_XID + ", "
+            + COLUMN_NAME_PERMISSION + " "
+            + "from " + TABLE_NAME + " where "
+            + COLUMN_NAME_ENTITY_XID + "=?;";
+
+    // @formatter:on
+
+    public List<UserPermission> getAllUsersPermissions() {
+        if (LOG.isTraceEnabled()) {
+            LOG.trace("getAllUsersPermissions()");
+        }
+
+        List<UserPermission> userPermissions = new ArrayList<>();
+
+        userPermissions = (List<UserPermission>) DAO.getInstance().getJdbcTemp().query(USER_PERMISSION_SELECT_ALL, new Object[]{}, new UserPermissionRowMapper() );
+
+        return userPermissions;
+    }
+
+    private class UserPermissionRowMapper implements RowMapper<UserPermission> {
+
+        @Override
+        public UserPermission mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+
+            UserPermission userPermission = new UserPermission();
+
+            userPermission.setId(resultSet.getInt(COLUMN_NAME_ID));
+            userPermission.setEntityXid(resultSet.getString(COLUMN_NAME_ENTITY_XID));
+            userPermission.setPermission(resultSet.getInt(COLUMN_NAME_PERMISSION));
+
+            return userPermission;
+        }
+    }
 
 }
