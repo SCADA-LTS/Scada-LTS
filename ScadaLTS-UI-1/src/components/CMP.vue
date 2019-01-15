0adbe242f37e744f82b97ff75a5b94d2ddef4fd4
Grzesiek Bylica <grzegorz.bylica@gmail.com>
2019-01-15 17:07:50 +0100

    #780 Added vue-underscore. Improvment create element to change state base on configuration.

--- ScadaLTS-UI-1/src/components/CMP.vue
+++ ScadaLTS-UI-1/src/components/CMP.vue
@@ -14,14 +14,20 @@
       <div class="well cmp_well_details_change">
         <section>
           <btn-group>
-
-            <div v-for="(item, index) in controls">
-              <btn block v-on:click="setAction(item.name)">{{item.name}}</btn>
+            <div v-for="(item, index) in controlsLevel0">
+              <btn block v-bind:class="{ selected: (selectActionLevel0==item.name)}" v-on:click="setActionLever0(item.name)">{{item.name}}</btn>
             </div>
-
+          </btn-group>
+          <btn-group>
+            <div v-if="controlsLevel1.length>0" v-for="(item, index) in controlsLevel1">
+              <btn block v-bind:class="{ selected: (selectActionLevel1==item.name)}" v-on:click="setActionLevel1(item.name)">{{item.name}}</btn>
+            </div>
+          </btn-group>
+          <btn-group>
+            <btn type="primary" v-on:click="tryChangeModePLC">Confirm changes</btn>
           </btn-group>
           <hr/>
-          <alert>Selected: {{selectAction}}</alert>
+          <alert>Selected: {{selectActionLevel0}} | {{selectActionLevel1}}</alert>
         </section>
       </div>
     </collapse>
@@ -34,11 +40,14 @@
 <script>
   import moment from "moment";
   import axios from 'axios';
+  import {_} from 'vue-underscore';
+  import BtnGroup from "uiv/src/components/button/BtnGroup";
 
   /**
    * @author grzegorz.bylica@gmail.com
    */
   export default {
+    components: {BtnGroup},
     props: ['pConfig', 'pLabel', 'pTimeRefresh'],
     data() {
       return {
@@ -48,43 +57,66 @@
         insideState: 'NOT-CHECKED',
         label: this.pLabel,
         timeRefresh: this.pTimeRefresh,
-        controls: [],
-        selectActionLevel1: 0,
-        selectActionLevel2: 0,
+        controlsLevel0: [],
+        controlsLevel1: [],
+        selectActionLevel0: '',
+        selectActionLevel1: '',
         fruits: [{name:'apple'}, {name:'banana'}, {name:'orange'}]
       }
     },
     methods: {
       checkStatus(){
-        this.config.state.analiseInOrder.forEach(function(entry){
+        Out:
+        for (let j=0; j<this.config.state.analiseInOrder.length;j++) {
+          let entry = this.config.state.analiseInOrder[j];
+
           //const apiCMPChek = `./api/cmp/check`;
           //axios.get(apiCMPChek).then(response => {
-          let response = {};
-          response.data = 0;
-          entry.toChecked.forEach(function(entryChecked){
-            const toRun = ""+response.data + entryChecked.equals;
 
-            if (eval(toRun)) {
-              console.log('test');
-              //this.insideState = entry.name;
-            };
-          });
+          let response = {};
+          response.data = 1;
+          for (let i=0; i<entry.toChecked.length; i++) {
+            let entryChecked = entry.toChecked[i];
+            try {
+              if (entryChecked.last == "true") {
+                this.insideState = entry.name;
+              } else {
+                let toRun = "" + response.data + entryChecked.equals;
+
+                if (eval(toRun)) {
+                  this.insideState = entry.name;
+                  break Out;
+                };
+                toRun = "";
+              }
+            } catch (e) {
+              console.log(e);
+            }
+          }
+
           //}).catch(error => {
           //  console.log(error);
           //});
 
-          //console.log(entry);
-        })
+        }
       },
-      setAction(action) {
-        alert(action);
+      setActionLever0(action) {
+        this.selectActionLevel0 = action;
+        let found = _.findWhere(this.controlsLevel0, {name:action});
+        this.controlsLevel1 = found.toChange;
+      },
+      setActionLevel1(action) {
+        this.selectActionLevel1 = action;
+      },
+      tryChangeModePLC() {
+        alert('try')
       }
     },
     created() {
 
       try {
         this.config = JSON.parse(this.strConfig);
-        this.controls = this.config.control.toChange;
+        this.controlsLevel0 = this.config.control.toChange;
       } catch (e) {
         console.log(e);
       }
@@ -135,6 +167,10 @@
   .cmp_well_details_change {
     margin-top: -3.2em;
     margin-bottom: 0;
+  }
+
+  .selected {
+    color: red;
   }
 
 </style>
