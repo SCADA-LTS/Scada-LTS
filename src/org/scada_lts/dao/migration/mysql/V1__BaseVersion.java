dddb37045cfe843fc6030ea83177ba4a02b36245
Grzegorz Bylica <grzegorz.bylica@gmail.com>
2019-07-15 12:46:23 +0200

    #942 changed creating default user, creationTimezone has been protected against empty values

--- src/org/scada_lts/dao/migration/mysql/V1__BaseVersion.java
+++ src/org/scada_lts/dao/migration/mysql/V1__BaseVersion.java
@@ -17,17 +17,12 @@
  */
 package org.scada_lts.dao.migration.mysql;
 
-import java.util.LinkedList;
-
 import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
 import org.scada_lts.dao.DAO;
 import org.scada_lts.dao.SystemSettingsDAO;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.serotonin.mango.Common;
-import com.serotonin.mango.db.dao.UserDao;
-import com.serotonin.mango.vo.User;
-import com.serotonin.mango.vo.permission.DataPointAccess;
 
 /**
  * @author grzegorz bylica Abil'I.T. development team, sdt@abilit.eu
@@ -724,26 +719,38 @@ public class V1__BaseVersion implements SpringJdbcMigration {
        
        if (!DAO.getInstance().isTest()) {
     	   // Create User
-	       User user = new User();
-	   	   user.setId(Common.NEW_ID);
-	   	   user.setUsername("admin");
-	   	   user.setPassword(Common.encrypt("admin"));
-	   	   user.setEmail("admin@yourMangoDomain.com");
-	       user.setHomeUrl("");
-	   	   user.setPhone("");
-	   	   user.setAdmin(true);
-	   	   user.setDisabled(false);
-	   	   user.setDataSourcePermissions(new LinkedList<Integer>());
-	   	   user.setDataPointPermissions(new LinkedList<DataPointAccess>());
-	   	   new UserDao().saveUser(user);
-	             	   
+
+		   String sqlUserAdd =
+			   "insert into users (" +
+				   "username, " +
+				   "password, " +
+				   "email, " +
+				   "phone, " +
+				   "admin, " +
+				   "disabled, " +
+				   "homeUrl, " +
+				   "receiveAlarmEmails, " +
+				   "receiveOwnAuditEvents) " +
+			   "values (?,?,?,?,?,?,?,?,?)";
+
+		   jdbcTemplate.update(sqlUserAdd, new Object[]{
+		   		"admin",
+				Common.encrypt("admin"),
+				"admin@yourDomain.com",
+				"",
+				DAO.boolToChar(true),
+				DAO.boolToChar(false),
+				"",
+				new Integer(0),
+				DAO.boolToChar(false)
+		   });
+
+
 	   	   // Record the current version.
 	   	   new SystemSettingsDAO().setValue(
 					SystemSettingsDAO.DATABASE_SCHEMA_VERSION,
 					Common.getVersion());
        }
-		   
-   	   
        //@formatter:on
     }
 
