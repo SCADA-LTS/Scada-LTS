ddb49e311b057fc10189461d3c062b44bf8179f3
Limraj <35842300+Limraj@users.noreply.github.com>
2019-08-27 19:40:24 +0200

    #960 - Config for Medium Priority Worker (ThreadPoolExecutor) to file properties

--- test/org/scada_lts/config/ScadaConfigTest.java
+++ test/org/scada_lts/config/ScadaConfigTest.java
@@ -30,8 +30,10 @@ import static org.junit.Assert.assertEquals;
 /**
  * Test config Scada-LTS
  * 
- * @author grzegorz bylica Abil'I.T. development team, sdt@abilit.eu
+ * @author grzegorz bylica Abil'I.T. development team, sdt@abilit.eu,
+ * kamil.jarmusik@gmail.com
  */
+
 public class ScadaConfigTest {
 
     private static ScadaConfig config;
@@ -52,10 +54,76 @@ public class ScadaConfigTest {
     }
 
     @Test
-    public void invoke_getInt_for_key_corePoolSize_return_3() {
+    public void invoke_getInt_for_key_ThreadPoolConfigKeys_corePoolSize_return_3() {
         //when:
-        int corePoolSize = config.getInt(ThreadPoolConfigKeys.CORE_POOL_SIZE, -1);
+        int corePoolSize = config
+                .getInt(ThreadPoolConfigKeys.CORE_POOL_SIZE, -1);
+
         //then:
         assertEquals(3, corePoolSize);
+    }
+
+    @Test
+    public void invoke_getInt_for_key_ThreadPoolConfigKeys_maximumPoolSize_return_100() {
+        //when:
+        int maximumPoolSize = config
+                .getInt(ThreadPoolConfigKeys.MAXIMUM_POOL_SIZE, -1);
+
+        //then:
+        assertEquals(100, maximumPoolSize);
+    }
+
+    @Test
+    public void invoke_getLong_for_key_ThreadPoolConfigKeys_keepAliveTime_return_60() {
+        //when:
+        long keepAliveTime = config
+                .getLong(ThreadPoolConfigKeys.KEEP_ALIVE_TIME,-1);
+        //then:
+        assertEquals(60, keepAliveTime);
+    }
+
+    @Test
+    public void invoke_getString_for_key_ThreadPoolConfigKeys_blockingQueueInterfaceImpl_return_LinkedBlockingQueue() {
+        //when:
+        String blockingQueueImplClassName = config
+                .getString(ThreadPoolConfigKeys.BLOCKING_QUEUE_INTERFACE_IMPL, "");
+        //then:
+        assertEquals("java.util.concurrent.LinkedBlockingQueue", blockingQueueImplClassName);
+    }
+
+    @Test
+    public void invoke_getString_for_key_ThreadPoolConfigKeys_timeUnitEnumValue_return_SECONDS() {
+        //when:
+        String timeUnitValue = config
+                .getString(ThreadPoolConfigKeys.TIME_UNIT_ENUM_VALUE, "");
+        //then:
+        assertEquals("SECONDS", timeUnitValue);
+    }
+
+    @Test
+    public void invoke_getString_for_key_SystemConfigKeys_aclServer_return_http_localhost_8090() {
+        //when:
+        String aclServer = config
+                .getString(SystemConfigKeys.ACL_SERVER, "");
+        //then:
+        assertEquals("http://localhost:8090", aclServer);
+    }
+
+    @Test
+    public void invoke_getString_for_key_SystemConfigKeys_croneUpdateCacheDataSourcesPoints_return_0_0_1() {
+        //when:
+        String croneUpdateCacheDataSourcesPoints = config
+                .getString(SystemConfigKeys.CRONE_UPDATE_CACHE_DATA_SOURCES_POINTS, "");
+        //then:
+        assertEquals("0 0/1 * * * ?", croneUpdateCacheDataSourcesPoints);
+    }
+
+    @Test
+    public void invoke_getString_for_key_SystemConfigKeys_croneUpdateCachePointHierarchy_return_0_0_10() {
+        //when:
+        String croneUpdateCachePointHierarchy = config
+                .getString(SystemConfigKeys.CRONE_UPDATE_CACHE_POINT_HIERARCHY, "");
+        //then:
+        assertEquals("0 0/10 * * * ?", croneUpdateCachePointHierarchy);
     }
 }
