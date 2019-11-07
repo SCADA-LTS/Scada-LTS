50ec24ca0cfeef4cb5c36dd4da296b09ab528f34
Grzegorz Bylica <grzegorz.bylica@gmail.com>
2019-11-07 15:21:41 +0100

    #1030 Added some example use components.

---
+++ scadalts-ui/src/views/components/ExampleExportImportPointHierarchy.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h4>Example of the use of the ExportImportPointHierarchy component</h4>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-4">
+                    Describe: <br/>A simple test to see if the value of the component is displayed.
+                </div>
+                <div class="col-xs-8">
+                    <ExportImportPointHierarchy/>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import ExportImportPointHierarchy from "../../components/point_hierarchy/ExportImportPointHierarchy"
+
+    export default {
+        name: 'example-ph',
+        components: {
+            ExportImportPointHierarchy
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ scadalts-ui/src/views/components/ExampleSleepAndReactivationDS.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h4>Example of the use of the SleepAndReactivationDS component</h4>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs-4">
+                    Describe: <br/>A simple test to see if the value of the component is displayed.
+                </div>
+                <div class="col-xs-8">
+                    <SleepAndReactivationDS/>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import SleepAndReactivationDS from "../../components/forms/SleepAndReactivationDS";
+
+    export default {
+        name: 'example-sleep-and-reactivation-ds',
+        components: {
+            SleepAndReactivationDS,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
