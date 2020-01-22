4710c5ece1a307047ff23b365a2385b34475a5c8
Limraj <35842300+Limraj@users.noreply.github.com>
2020-01-22 15:34:51 +0100

    #1100 - Corrected the script get_seroUtils.sh - SeroUtils from GitHub

--- get_seroUtils.sh
+++ get_seroUtils.sh
@@ -1,5 +1,15 @@
-#!/bin/bash
+#!/bin/bash -x
 
-curl --header 'Host: netcologne.dl.sourceforge.net' --user-agent 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' --header 'Accept-Language: en-GB,en;q=0.5' --referer 'https://sourceforge.net/projects/scadabr/files/Dependency/seroUtils.jar/download' --cookie '_ga=GA1.2.1965164509.1526549518; _gid=GA1.2.67519373.1526549518; __gads=ID=760cd624c839f515:T=1526549520:S=ALNI_MYLZ8YQgYn3PztCF8z30TJ_fUjVdA; _scp=1526549551658.1602999522; _scs=1526549551659.326419338' --header 'Upgrade-Insecure-Requests: 1' 'https://netcologne.dl.sourceforge.net/project/scadabr/Dependency/seroUtils.jar' --output 'seroUtils.jar'
+sha256() {
+    curl -LJO https://github.com/SCADA-LTS/SeroUtils/releases/download/1.0.0/seroUtils.jar
+    curl -o seroUtils.json https://api.github.com/repos/SCADA-LTS/SeroUtils/releases
+    online_sha256=$(grep -oP '(?<="body": ")[^"]*' seroUtils.json)
+    local_sha256="$(cat seroUtils.jar | sha256sum | head -c 64)"
+}
 
-mv seroUtils.jar ./WebContent/WEB-INF/lib/
+i=0
+while : ; do
+    sha256
+    i=$(( i + 1 ))
+    [ "$i" -lt 3 ] && [ ! -e seroUtils.jar ] && [ "$online_sha256" != "$local_sha256" ] || break
+done
