5e1712f9869190448a52d117a6c55b95c60090f0
mateuszhyski <hyski.mateusz@gmail.com>
2020-03-12 16:51:47 +0100

    - save actual work - junit

--- test/org/scada_lts/dao/PointValueDAOTest.java
+++ test/org/scada_lts/dao/PointValueDAOTest.java
@@ -5,12 +5,15 @@ import org.scada_lts.dao.pointvalues.PointValueDAO;
 import org.scada_lts.dao.pointvalues.ResultSetImpl;
 
 import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PointValueDAOTest extends TestDAO {
 
     @Test
     public void test1(){
-        ResultSet resultSet = new ResultSetImpl();
+        Map<String,String> rowExample = new HashMap<String,String>();
+        ResultSet resultSet = new ResultSetImpl(rowExample);
 
         new PointValueDAO().getPointValueRow(resultSet,1);
 
