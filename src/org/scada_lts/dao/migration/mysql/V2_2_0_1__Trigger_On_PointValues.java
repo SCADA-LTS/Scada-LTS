5fb85b203cb6339ec663b6f7c783527150a4d585
mateuszhyski <hyski.mateusz@gmail.com>
2020-04-30 17:18:58 +0200

    #1164 trigger - body of this is this ongoing - update by flyway works :)

--- src/org/scada_lts/dao/migration/mysql/V2_2_0_1__Trigger_On_PointValues.java
+++ src/org/scada_lts/dao/migration/mysql/V2_2_0_1__Trigger_On_PointValues.java
@@ -9,9 +9,36 @@ import org.springframework.jdbc.core.JdbcTemplate;
 public class V2_2_0_1__Trigger_On_PointValues implements SpringJdbcMigration {
 
     public void migrate(JdbcTemplate jdbcTmp) throws Exception {
-        final String addTrigger = "";
-
-
-       // jdbcTmp.execute(addTrigger);
+        jdbcTmp.execute( ""
+        +"CREATE TRIGGER event_name AFTER INSERT "
+        +"ON pointValues "
+        +"FOR EACH ROW BEGIN "
+        +"DECLARE VARIABLE1 VARCHAR(100); "
+        +"DECLARE VARIABLE2 VARCHAR(100); "
+        +"SET VARIABLE1 = (SELECT UNIX_TIMESTAMP(NOW())); "
+        +"SET VARIABLE2 = (select xid from dataPoints where id=new.dataPointId); "
+        +"SET VARIABLE2 = (select if(LOCATE('AL',VARIABLE2) <> 0 ,'AL', if(LOCATE('ST',VARIABLE2) <> 0 ,'ST','undefined'))); "
+        +""
+        +"insert into pointValuesStorungsAndAlarms ("
+        +"pointId,"
+        +"pointXid,"
+        +"pointType,"
+        +"pointName,"
+        +"triggerTime,"
+        +"inactiveTime,"
+        +"acknowledgeTime,"
+        +"lastpointValue"
+        +") values ("
+         +" (select id from dataPoints where id=new.dataPointId),"
+         +" (select xid from dataPoints where id=new.dataPointId),"
+         +"'TYP',"
+         +" (select xid from dataPoints where id=new.dataPointId),"
+        +" 'event_name',"
+        +" 'inactiveTime',"
+        +" 'acknowledgeTime',"
+        +" 'lastpointValue'"
+        +"); "
+        +"SET VARIABLE2 = (SELECT UNIX_TIMESTAMP(NOW()));"
+                +"end; ");
     }
 }
