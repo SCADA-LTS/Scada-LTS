80c2ada4a5f4986be3d95a0d12d9f412605ba84a
mateuszhyski <hyski.mateusz@gmail.com>
2020-05-08 13:02:28 +0200

    #1178 additional views regarding email Grzegorz - every view in separated class - save work

---
+++ src/org/scada_lts/dao/migration/mysql/V2_2_1_1__View_ApiAlarmsLive_For_PointValueWarningsAndAlarms.java
@@ -0,0 +1,26 @@
+package org.scada_lts.dao.migration.mysql;
+
+import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+/**
+ * @author  hyski mateusz@gmail.com on 07.05.2020
+ */
+public class V2_2_1_1__View_ApiAlarmsLive_For_PointValueWarningsAndAlarms implements SpringJdbcMigration {
+
+    public void migrate(JdbcTemplate jdbcTmp) throws Exception {
+
+
+        //view for Rest API  -> /api/alarms/live
+
+        try {
+            jdbcTmp.execute(""
+                    + " create view apiAlarmsLive as"
+                    + "  select id,activationTime as 'activation-time',inactivationTime as 'inactivation-time, name, ;level from plcAlarms");
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+        //        + "  select id,activationTime as activation-Time, inactivationTime as inactivation-Time,name,level* from pointValues
+
+    }
+}
