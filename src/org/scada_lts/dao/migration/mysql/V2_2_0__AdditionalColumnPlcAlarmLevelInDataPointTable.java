39c3c41b24ac2e321c7943866d18da07c9f94787
mateuszhyski <hyski.mateusz@gmail.com>
2020-05-12 15:34:34 +0200

    #1211 for safe merge to #1197

---
+++ src/org/scada_lts/dao/migration/mysql/V2_2_0__AdditionalColumnPlcAlarmLevelInDataPointTable.java
@@ -0,0 +1,22 @@
+package org.scada_lts.dao.migration.mysql;
+
+import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * @author  hyski mateusz@gmail.com on 27.04.2020
+ */
+public class V2_2_0__AdditionalColumnPlcAlarmLevelInDataPointTable  implements SpringJdbcMigration {
+
+    public void migrate(JdbcTemplate jdbcTmp) throws Exception {
+
+        try {
+            jdbcTmp.execute(""
+                    + "ALTER TABLE dataPoints " +
+                    "ADD COLUMN `plcAlarmLevel` TINYINT(8) NULL AFTER `data`;");//this additional column will have defined level of alarm as a 0-8 steps.
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
