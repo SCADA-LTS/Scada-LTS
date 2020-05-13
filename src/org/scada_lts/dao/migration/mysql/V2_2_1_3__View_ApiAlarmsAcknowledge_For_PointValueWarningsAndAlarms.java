7fabee99c12a6293bb500e253a4be5d0a0f5a402
mateusz <nnnowicjuszszsz@gmail.com>
2020-05-13 21:17:20 +0200

    #1178 labels.

--- src/org/scada_lts/dao/migration/mysql/V2_2_1_3__View_ApiAlarmsAcknowledge_For_PointValueWarningsAndAlarms.java
+++ src/org/scada_lts/dao/migration/mysql/V2_2_1_3__View_ApiAlarmsAcknowledge_For_PointValueWarningsAndAlarms.java
@@ -1,5 +1,21 @@
 package org.scada_lts.dao.migration.mysql;
-
+/*
+ * (c) 2020 hyski.mateusz@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -15,9 +31,16 @@ public class V2_2_1_3__View_ApiAlarmsAcknowledge_For_PointValueWarningsAndAlarms
         //view for Rest API  -> /api/alarms/acknowledge
 
         try {
-            jdbcTmp.execute(""
-                    + " create view apiAlarmsAcknowledge as"
-                    + "  select id,pointType as 'request',pointType as 'error' from plcAlarms");
+            jdbcTmp.execute(
+                    new CreateView().
+                            CreateViewWithSpecification(
+                                    new StringBuilder("apiAlarmsAcknowledge"),
+                                    new String[]{
+                                            "id,",
+                                            "pointType as 'request',",
+                                            "pointType as 'error'"},
+                                    new StringBuilder("plcAlarms")
+                            ));
         } catch (DataAccessException e) {
             e.printStackTrace();
         }
