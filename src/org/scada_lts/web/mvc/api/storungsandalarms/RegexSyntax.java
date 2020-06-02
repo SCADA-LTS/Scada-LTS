24aa81c7c7a99410f93af1c5f80ae748098eff87
mateuszhyski <hyski.mateusz@gmail.com>
2020-06-02 16:06:07 +0200

    #1182 code up and check with fault cases

--- src/org/scada_lts/web/mvc/api/storungsandalarms/RegexSyntax.java
+++ src/org/scada_lts/web/mvc/api/storungsandalarms/RegexSyntax.java
@@ -1,6 +1,30 @@
 package org.scada_lts.web.mvc.api.storungsandalarms;
-
+/*
+ * (c) 2020 hyski.mateusz@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+/**
+ * Create by at Mateusz Hyski
+ *
+ * Contain ONLY regex definitions
+ *
+ *
+ * @author hyski.mateusz@gmail.com 02-06-2020
+ */
 class RegexSyntax {
 
-    final static String VALUE_BEWTEEN_0_AND_9999 = "[^a-z]|[^A-Z]|[0-9]{1,4}";
+    final static String VALUE_BETWEEN_0_AND_9999 = "[^a-z]|[^A-Z]|[0-9]{1,4}";
 }
--- src/org/scada_lts/web/mvc/api/storungsandalarms/Validation.java
+++ src/org/scada_lts/web/mvc/api/storungsandalarms/Validation.java
@@ -19,13 +19,7 @@ package org.scada_lts.web.mvc.api.storungsandalarms;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * Create by at Mateusz Hyski
@@ -36,7 +30,7 @@ class Validation {
 
     protected static final Log LOG = LogFactory.getLog(Validation.class);
     static String validateDoParamIsIntegerAndBetween0And9999(String paramName,String param) {
-        if( !param.matches(RegexSyntax.VALUE_BEWTEEN_0_AND_9999)){
+        if( !param.matches(RegexSyntax.VALUE_BETWEEN_0_AND_9999)){
             return "Value "+paramName+" is not correct.It should be a number beetwen 0 and 9999.";
         }
         return null;
