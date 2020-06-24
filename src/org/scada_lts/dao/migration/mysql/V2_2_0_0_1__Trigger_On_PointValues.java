1ba344b29ae9ba1be7bc91c2a3ea7c547fb414a6
mateuszhyski <hyski.mateusz@gmail.com>
2020-06-24 15:33:24 +0200

    #1197 update logic - when alarm/storung really, is, and gone

--- src/org/scada_lts/dao/migration/mysql/V2_2_0_0_1__Trigger_On_PointValues.java
+++ src/org/scada_lts/dao/migration/mysql/V2_2_0_0_1__Trigger_On_PointValues.java
@@ -35,7 +35,6 @@ public class V2_2_0_0_1__Trigger_On_PointValues implements SpringJdbcMigration {
                     +"DECLARE PLC_ALARM_LEVEL INT(1); "
                     +"DECLARE LAST_POINT_VALUE INT(1); "
                     +"DECLARE PRESENT_POINT_VALUE INT(1); "
-                    +"DECLARE ALARM_STORUNG_FOR_POINT_WITH_SPECIFIC_NAME_AND_EXISTED_LOW_STATE_EXIST INT(1); "
                     +"DECLARE ALARM_STORUNG_FOR_POINT_WITH_SPECIFIC_NAME_AND_WITH_STATE_HIGH_EXIST_IN_PLCALARM INT(1); "
                     +"DECLARE actualIdRow INT(10); "
                     +"DECLARE ALARM_IS_GEGANGEN VARCHAR(40) DEFAULT 'Alarm is gegangen'; "
@@ -43,43 +42,24 @@ public class V2_2_0_0_1__Trigger_On_PointValues implements SpringJdbcMigration {
                     +"DECLARE ALARM_AUSGELOST VARCHAR(40) DEFAULT 'Alarm ausgelost'; "
                     +"DECLARE STORUNG_KOMMT VARCHAR(40) DEFAULT 'Storung kommt'; "
                     +"DECLARE EMPTY_STRING VARCHAR(40) DEFAULT ' '; "
+                    +"DECLARE DESCRIPTION_FOR_FIRST_INSERT VARCHAR(40) DEFAULT ' '; "
 
                     // TRIGGER_TIME means, when this trigger has been executed
                     +"DECLARE TRIGGER_TIME VARCHAR(20); "
                     +""
-                    +""
                     +" select plcAlarmLevel into PLC_ALARM_LEVEL from dataPoints where id=new.dataPointId;  "
                     +""
-                    +" select pointValue into LAST_POINT_VALUE from pointValues where ts=(select max(ts) from pointValues where dataPointId=new.dataPointId); "
-                    +""
-                    +" select count(*) into ALARM_STORUNG_FOR_POINT_WITH_SPECIFIC_NAME_AND_EXISTED_LOW_STATE_EXIST from plcAlarms where pointName=(select pointName from dataPoints where id=new.dataPointId) and state=0; "
                     +" select count(*) into ALARM_STORUNG_FOR_POINT_WITH_SPECIFIC_NAME_AND_WITH_STATE_HIGH_EXIST_IN_PLCALARM from plcAlarms where pointName=(select pointName from dataPoints where id=new.dataPointId) and state=1; "
                     +""
                     +" select from_unixtime(unix_timestamp()) into TRIGGER_TIME; "
                     +" select new.pointValue into PRESENT_POINT_VALUE; "
                     +""
-                    // PLC_ALARM_LEVEL - 1 -> for dta point defined as Alarm
+
+                    // PLC_ALARM_LEVEL - 1 -> for data points defined as Alarm
                     // PLC_ALARM_LEVEL - 2 -> for data points defined as Storungs
 
                     +" IF (PLC_ALARM_LEVEL = 1 OR PLC_ALARM_LEVEl = 2) THEN "
                     + ""
-                    + " IF (ALARM_STORUNG_FOR_POINT_WITH_SPECIFIC_NAME_AND_EXISTED_LOW_STATE_EXIST=1) THEN "
-                    +""
-                    + "     select id into actualIdRow from plcAlarms where pointName=(select pointName from dataPoints where id=new.dataPointId) and state=0; "
-                    +""
-                    //update also description regarding a state which has been changed
-                    +"      IF (PLC_ALARM_LEVEL=1 ) THEN "
-                    +"          update plcAlarms set description=ALARM_AUSGELOST where id=actualIdRow;  "
-                    +"      END IF; "
-
-                    // fill specific column with information that an storung has come on--
-                    +"      IF (PLC_ALARM_LEVEL=2) THEN "
-                    +"          update plcAlarms set description=STORUNG_KOMMT where id=actualIdRow;   "
-                    +"      END IF; "
-                    +""
-                    +"      update plcAlarms set state=1,triggerTime=TRIGGER_TIME where id=actualIdRow;  "
-                    +""
-                    +"  END IF;"
 
                     +" IF (ALARM_STORUNG_FOR_POINT_WITH_SPECIFIC_NAME_AND_WITH_STATE_HIGH_EXIST_IN_PLCALARM=1) THEN"
                     +""
@@ -93,10 +73,22 @@ public class V2_2_0_0_1__Trigger_On_PointValues implements SpringJdbcMigration {
                     +"          update plcAlarms set description=STORUNG_IS_GEGANGEN where id=actualIdRow; "
                     +"      END IF; "
 
+                    //update the state regarding a state which has been changed
                     +"      update plcAlarms set state=2,inactiveTime=TRIGGER_TIME where id=actualIdRow; "
                     +" END IF;"
 
-                    +" IF (PRESENT_POINT_VALUE=0) THEN"
+                    +" IF (PRESENT_POINT_VALUE=1) THEN"
+                    +""
+                    // first define correct description which is needed for appeared state
+
+                    +"      IF (PLC_ALARM_LEVEL = 1) THEN "
+                    +"          SET DESCRIPTION_FOR_FIRST_INSERT = ALARM_AUSGELOST; "
+                    +"      END IF; "
+                    +"      IF (PLC_ALARM_LEVEL = 2) THEN "
+                    +"          SET DESCRIPTION_FOR_FIRST_INSERT = STORUNG_KOMMT; "
+                    +"      END IF; "
+
+
                     +"  insert into plcAlarms "
                     +" ("
                     +"      pointId,"
@@ -118,12 +110,12 @@ public class V2_2_0_0_1__Trigger_On_PointValues implements SpringJdbcMigration {
                     +"  PLC_ALARM_LEVEL,"
                     +"  (select pointName from dataPoints where id=new.dataPointId),"
                     +"  TRIGGER_TIME,"
+                    +"  TRIGGER_TIME,"
                     +"  EMPTY_STRING,"
                     +"  EMPTY_STRING,"
-                    +"  (select max(ts) from pointValues where dataPointId=new.dataPointId),"
-                    +"  LAST_POINT_VALUE,"
-                    +"  EMPTY_STRING,"
-                    +"  0"
+                    +"  PRESENT_POINT_VALUE,"
+                    +"  DESCRIPTION_FOR_FIRST_INSERT,"
+                    +"  1"
                     +"); "
                     +""
                     +" END IF;"
