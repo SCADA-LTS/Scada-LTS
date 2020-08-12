ff101efc91be5b20b44c1a7446588a8e966b7456
Grzegorz Bylica <grzegorz.bylica@gmail.com>
2020-08-12 12:07:05 +0200

    #1349 Added i18n and config and added SR for view About

--- scadalts-ui/src/views/About.vue
+++ scadalts-ui/src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ $t('about') }}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'AboutI18n'
+}
+</script>
+
+<i18n>
+{
+  "en": {
+    "about": "This is an about page (i18n test)"
+  }
+}
+</i18n>
