a74159db9ad8a620f2c1cef9dbdbc53daf90dc4b
Grzegorz Bylica <grzegorz.bylica@gmail.com>
2020-09-15 11:29:32 +0200

    #1376 Correcting migration 2.2, 2.3
        I can delete previous migrations in a new migration by using the sql command directly and it will only work in version 2.3.

--- src/org/scada_lts/dao/migration/mysql/V2_3__FaultsAndAlarms.java
+++ src/org/scada_lts/dao/migration/mysql/V2_3__FaultsAndAlarms.java
@@ -48,6 +48,53 @@ public class V2_3__FaultsAndAlarms extends BaseJavaMigration {
 
         final JdbcTemplate jdbcTmp = DAO.getInstance().getJdbcTemp();
 
+        jdbcTmp.execute("DROP VIEW IF EXISTS apiAlarmsAcknowledge;");
+        //(1)
+        jdbcTmp.execute( "DELETE FROM schema_version WHERE version = '2.2.1.3';");
+
+        jdbcTmp.execute("DROP VIEW IF EXISTS apiAlarmsHistory;");
+        //(2)
+        jdbcTmp.execute( "DELETE FROM schema_version WHERE version = '2.2.1.2';");
+
+        jdbcTmp.execute("DROP PROCEDURE IF EXISTS prc_sort_alarms_and_storungs_depend_on_state;");
+        jdbcTmp.execute("DROP TEMPORARY TABLE IF EXISTS tmp_sortedAlarmsStorungs;");
+        //(3)
+        jdbcTmp.execute( "DELETE FROM schema_version WHERE version = '2.2.1.1.1';");
+
+        jdbcTmp.execute("DROP VIEW IF EXISTS apiAlarmsLive;");
+        //(4)
+        jdbcTmp.execute( "DELETE FROM schema_version WHERE version = '2.2.1.1';");
+
+        jdbcTmp.execute("DROP VIEW IF EXISTS viewAllStorungs;");
+        jdbcTmp.execute("DROP VIEW IF EXISTS viewAllAlarms;");
+        //(5)
+        jdbcTmp.execute( "DELETE FROM schema_version WHERE version = '2.2.1';");
+
+        try {
+            jdbcTmp.execute("ALTER TABLE dataPoints DROP COLUMN pointName;");
+        } catch (Exception e) {
+            LOG.warn(e);
+        }
+        //(6)
+        jdbcTmp.execute( "DELETE FROM schema_version WHERE version = '2.2.0.2';");
+
+        jdbcTmp.execute("DROP TABLE IF EXISTS plcAlarms;");
+        //(7)
+        jdbcTmp.execute( "DELETE FROM schema_version WHERE version = '2.2.0.1';");
+
+        jdbcTmp.execute( "DROP TRIGGER IF EXISTS onlyForStorungsAndAlarmValues;");
+        //(8)
+        jdbcTmp.execute( "DELETE FROM schema_version WHERE version = '2.2.0.0.1';");
+
+        try {
+            jdbcTmp.execute("ALTER TABLE dataPoints DROP COLUMN plcAlarmLevel;");
+        } catch (Exception e) {
+            LOG.warn(e);
+        }
+        //(9)
+        jdbcTmp.execute( "DELETE FROM schema_version WHERE version = '2.2.0';");
+
+
         addColumnsToDataPointsTable(jdbcTmp);
         updateDataPointsTable(jdbcTmp);
         createPlcAlarmsTable(jdbcTmp);
