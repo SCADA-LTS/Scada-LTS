ca8d6913db3ce4c2b59dccfe42943f5be52acab5
Radek Jajko <radek235s@gmail.com>
2020-10-15 10:21:01 +0200

    Fix: Vue components are rendered (but only Hello World)
    While upgrading to Vue3 most of additional libraries does not work correctly. So most of them has been disabled for just run the application.
    #1411

--- scadalts-ui/src/apps/App.vue
+++ scadalts-ui/src/apps/App.vue
@@ -1,5 +1,6 @@
 <template>
-    <div id="app">
+  <div>Hello World!</div>
+  <!-- <div id="app">
         <input type="checkbox" id="check-sidebar" class="nav-sidebar">
         <label for="check-sidebar" class="nav-sidebar">
             <i id="btn-sidebar"><font-awesome-icon icon="bars" /></i>
@@ -27,177 +28,176 @@
         <div class="view">
             <router-view></router-view>
         </div>
-    </div>
-
+    </div> -->
 </template>
 
 <script>
-
-    export default {
-        name: 'app',
-        components: {
-        }
-    }
+export default {
+  name: "App",
+  components: {},
+};
 </script>
 
 <style scoped>
-   /* @import '../../node_modules/ag-grid-community/dist/styles/ag-grid.css';
+/* @import '../../node_modules/ag-grid-community/dist/styles/ag-grid.css';
     @import '../../node_modules/ag-grid-community/dist/styles/ag-theme-balham.css';*/
 
+:root {
+  font-size: 16px;
+  font-family: OpenSans-Regular;
+}
+* {
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+}
+.sidebar {
+  z-index: 10;
+  position: fixed;
+  margin-top: -20px;
+  left: -240px;
+  width: 240px;
+  height: 100%;
+  background-color: #141418;
+  transition: all 0.5s ease;
+}
+.sidebar logo {
+  display: block;
+  color: lightgray;
+  font-size: 1.4rem;
+  text-align: center;
+  background: #1b1b1b;
+  user-select: none;
+  line-height: 3rem;
+}
+.sidebar a {
+  display: block;
+  color: lightgray;
+  height: 2.4rem;
+  line-height: 2.4rem;
+  padding-left: 1.6rem;
+  width: 90%;
+}
+.sidebar a:hover,
+a.active {
+  background: #2e486d;
+  width: 240px;
+}
+.sidebar a i {
+  font-size: 1.2rem;
+  margin-right: 1.6rem;
+}
+.sidebar a span {
+  letter-spacing: 1px;
+  text-transform: uppercase;
+}
+#check-sidebar {
+  display: none;
+}
+navnui {
+  z-index: 5;
+  margin-top: -20px;
+  position: fixed;
+  width: 100%;
+  height: 42px;
+  background-color: #23232e;
+  color: lightgrey;
+}
+.navnui-left {
+  float: left;
+  margin-left: 80px;
+}
+.navnui-right {
+  float: right;
+}
 
-   :root {
-       font-size: 16px;
-       font-family: OpenSans-Regular;
-   }
-   * {
-       margin: 0;
-       padding: 0;
-       text-decoration: none;
-   }
-   .sidebar{
-       z-index: 10;
-       position: fixed;
-       margin-top: -20px;
-       left: -240px;
-       width: 240px;
-       height: 100%;
-       background-color: #141418;
-       transition: all .5s ease;
-   }
-   .sidebar logo {
-       display: block;
-       color: lightgray;
-       font-size: 1.4rem;
-       text-align: center;
-       background: #1b1b1b;
-       user-select: none;
-       line-height: 3rem;
-   }
-   .sidebar a {
-       display: block;
-       color: lightgray;
-       height: 2.4rem;
-       line-height: 2.4rem;
-       padding-left: 1.6rem;
-       width: 90%;
-   }
-   .sidebar a:hover, a.active {
-       background: #2e486d;
-       width: 240px;
-   }
-   .sidebar a i {
-       font-size: 1.2rem;
-       margin-right: 1.6rem;
-   }
-   .sidebar a span{
-       letter-spacing: 1px;
-       text-transform: uppercase;
-   }
-   #check-sidebar {
-       display: none;
-   }
-   navnui {
-       z-index: 5;
-       margin-top: -20px;
-       position: fixed;
-       width: 100%;
-       height: 42px;
-       background-color: #23232e;
-       color: lightgrey;
-   }
-   .navnui-left {
-       float:left;
-       margin-left: 80px;
-   }
-   .navnui-right {
-       float:right;
-   }
+header::after {
+  content: "";
+  display: table;
+  clear: both;
+}
+navnui ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+navnui li {
+  float: left;
+  display: inline-block;
+  margin: 10px 10px 30px;
+}
 
-   header::after {
-       content: '';
-       display: table;
-       clear: both;
-   }
-   navnui ul {
-       margin: 0;
-       padding: 0;
-       list-style: none;
-   }
-   navnui li {
-       float: left;
-       display: inline-block;
-       margin: 10px 10px 30px;
-   }
+navnui a {
+  color: #b6b6b6;
+}
 
-   navnui a {
-       color: #b6b6b6;
-   }
+label #btn-sidebar,
+label #cancel-sidebar {
+  z-index: 6;
+  position: absolute;
+  cursor: pointer;
+  height: 37px;
+  width: 37px;
+  text-align: center;
+  color: white;
+  font-size: 24px;
+  margin: -12px 30px 10px 30px;
+  transition: all 0.5s ease;
+}
 
-   label #btn-sidebar, label #cancel-sidebar {
-       z-index: 6;
-       position: absolute;
-       cursor: pointer;
-       height: 37px;
-       width: 37px;
-       text-align: center;
-       color: white;
-       font-size: 24px;
-       margin: -12px 30px 10px 30px;
-       transition: all .5s ease;
-   }
+label #cancel-sidebar {
+  opacity: 0;
+  visibility: hidden;
+}
+#check-sidebar:checked ~ label #btn-sidebar {
+  margin-left: 245px;
+  opacity: 0;
+  visibility: hidden;
+}
+#check-sidebar:checked ~ label #cancel-sidebar {
+  margin-left: 245px;
+  opacity: 1;
+  visibility: visible;
+}
+#check-sidebar:checked ~ .sidebar {
+  left: 0;
+}
 
-   label #cancel-sidebar {
-       opacity:0;
-       visibility: hidden;
-   }
-   #check-sidebar:checked ~ label #btn-sidebar {
-       margin-left: 245px;
-       opacity: 0;
-       visibility: hidden;
-   }
-   #check-sidebar:checked ~ label #cancel-sidebar {
-       margin-left: 245px;
-       opacity: 1;
-       visibility: visible;
-   }
-   #check-sidebar:checked ~ .sidebar{
-       left:0;
-   }
-
-   #check-sidebar:checked ~ #navigation-left {
-       margin-left: 310px;
-   }
-   @media (max-width:860px) {
-       .sidebar{
-           height: auto;
-           width: 55px;
-           left: 0;
-           margin: 100px 0;
-       }
-       .sidebar logo, #btn-sidebar, #cancel-sidebar {
-           display: none;
-       }
-       span {
-           position: absolute;
-           margin-left: 30px;
-           opacity: 0;
-           visibility: hidden;
-       }
-       .sidebar a i {
-           margin-left: -15px;
-       }
-       a:hover{
-           width: 240px;
-           background: inherit;
-       }
-       .sidebar a:hover span {
-           opacity: 1;
-           visibility: visible;
-       }
-       .sidebar a.active {
-           background: #2e486d;
-           width: 240px !important;
-       }
-   }
-
+#check-sidebar:checked ~ #navigation-left {
+  margin-left: 310px;
+}
+@media (max-width: 860px) {
+  .sidebar {
+    height: auto;
+    width: 55px;
+    left: 0;
+    margin: 100px 0;
+  }
+  .sidebar logo,
+  #btn-sidebar,
+  #cancel-sidebar {
+    display: none;
+  }
+  span {
+    position: absolute;
+    margin-left: 30px;
+    opacity: 0;
+    visibility: hidden;
+  }
+  .sidebar a i {
+    margin-left: -15px;
+  }
+  a:hover {
+    width: 240px;
+    background: inherit;
+  }
+  .sidebar a:hover span {
+    opacity: 1;
+    visibility: visible;
+  }
+  .sidebar a.active {
+    background: #2e486d;
+    width: 240px !important;
+  }
+}
 </style>
--- scadalts-ui/src/router.js
+++ scadalts-ui/src/router.js
@@ -1,79 +1,78 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Alarms from './views/Alarms'
-import About from './views/About'
-import HistoricalAlarms from "./views/HistoricalAlarms"
+// import { createRouter, createWebHashHistory } from 'vue-router'
+// // import Alarms from './views/Alarms'
+// // import About from './views/About'
+// // import HistoricalAlarms from "./views/HistoricalAlarms"
+// // import App from "./apps/App"
 
-Vue.use(Router)
+// const routes = [
+//   // {
+//   //   path: '/',
+//   //   name: 'home',
+//   //   component: App
+//   // },
+//   // {
+//   //   path: '/about',
+//   //   name: 'about',
+//   //   component: About
+//   // },
+//   // {
+//   //   path: '/alarms',
+//   //   name: 'alarms',
+//   //   component: Alarms
+//   // },
+//   // {
+//   //   path: '/historical-alarms',
+//   //   name: 'historical-alarms',
+//   //   component: HistoricalAlarms
+//   // },
+//   // {
+//   //   path: '/example-ph',
+//   //   name: 'example-ph',
+//   //   component: () => import(/* webpackChunkName: "ph" */ './views/components/ExampleExportImportPointHierarchy.vue')
+//   // },
+//   // {
+//   //   path: '/example-test',
+//   //   name: 'example-test',
+//   //   component: () => import(/* webpackChunkName: "test-component" */ './views/components/ExampleTest.vue')
+//   // },
+//   // {
+//   //   path: '/example-isalive',
+//   //   name: 'example-isalive',
+//   //   component: () => import(/* webpackChunkName: "isalive-component" */ './views/components/ExampleIsAlive.vue')
+//   // },
+//   // {
+//   //   path: '/example-simple-component-svg',
+//   //   name: 'example-simple-component-svg',
+//   //   component: () => import(/* webpackChunkName: "simple-component-svg" */ './views/components/ExampleSimpleComponentSVG.vue')
+//   // },
+//   // {
+//   //   path: '/example-sleep-and-reactivation-ds',
+//   //   name: 'example-sleep-and-reactivation-ds',
+//   //   component: () => import(/* webpackChunkName: "sleep-and-reactivation-ds-component" */ './views/components/ExampleSleepAndReactivationDS.vue')
+//   // },
+//   // {
+//   //   path: '/example-cmp',
+//   //   name: 'example-cmp',
+//   //   component: () => import(/* webpackChunkName: "cmp-component" */ './views/components/ExampleCmp.vue')
+//   // },
+//   // {
+//   //   path: '/example-chart-cmp',
+//   //   name: 'example-chart-cmp',
+//   //   component: () => import(/* webpackChunkName: "example-chart-cmp" */ './views/components/ExampleChartCmp.vue')
+//   // },
+//   // {
+//   //   path: '/example-step-line-chart-cmp',
+//   //   name: 'example-step-line-chart-cmp',
+//   //   component: () => import(/* webpackChunkName: "step-line-chart-component" */ './views/components/ExampleStepLineChartCmp.vue')
+//   // },
+//   // {
+//   //   path: '/example-live-alarms',
+//   //   name: 'example-live-alarms',
+//   //   component: () => import(/* webpackChunkName: "live-alarms-component" */ './views/components/ExampleLiveAlarms.vue')
+//   // },
+// ]
 
-export default new Router({
-  mode: 'hash',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Alarms
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About
-    },
-    {
-      path: '/alarms',
-      name: 'alarms',
-      component: Alarms
-    },
-    {
-      path: '/historical-alarms',
-      name: 'historical-alarms',
-      component: HistoricalAlarms
-    },
-    {
-      path: '/example-ph',
-      name: 'example-ph',
-      component: () => import(/* webpackChunkName: "ph" */ './views/components/ExampleExportImportPointHierarchy.vue')
-    },
-    {
-      path: '/example-test',
-      name: 'example-test',
-      component: () => import(/* webpackChunkName: "test-component" */ './views/components/ExampleTest.vue')
-    },
-    {
-      path: '/example-isalive',
-      name: 'example-isalive',
-      component: () => import(/* webpackChunkName: "isalive-component" */ './views/components/ExampleIsAlive.vue')
-    },
-    {
-      path: '/example-simple-component-svg',
-      name: 'example-simple-component-svg',
-      component: () => import(/* webpackChunkName: "simple-component-svg" */ './views/components/ExampleSimpleComponentSVG.vue')
-    },
-    {
-      path: '/example-sleep-and-reactivation-ds',
-      name: 'example-sleep-and-reactivation-ds',
-      component: () => import(/* webpackChunkName: "sleep-and-reactivation-ds-component" */ './views/components/ExampleSleepAndReactivationDS.vue')
-    },
-    {
-      path: '/example-cmp',
-      name: 'example-cmp',
-      component: () => import(/* webpackChunkName: "cmp-component" */ './views/components/ExampleCmp.vue')
-    },
-    {
-      path: '/example-chart-cmp',
-      name: 'example-chart-cmp',
-      component: () => import(/* webpackChunkName: "example-chart-cmp" */ './views/components/ExampleChartCmp.vue')
-    },
-    {
-      path: '/example-step-line-chart-cmp',
-      name: 'example-step-line-chart-cmp',
-      component: () => import(/* webpackChunkName: "step-line-chart-component" */ './views/components/ExampleStepLineChartCmp.vue')
-    },
-    {
-      path: '/example-live-alarms',
-      name: 'example-live-alarms',
-      component: () => import(/* webpackChunkName: "live-alarms-component" */ './views/components/ExampleLiveAlarms.vue')
-    },
-  ]
-})
+// export default createRouter({
+//   history: createWebHashHistory(),
+//   routes
+// });
