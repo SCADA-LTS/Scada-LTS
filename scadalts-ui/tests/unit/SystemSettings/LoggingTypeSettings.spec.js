135f7fe76c32de4186b2df58c86889d8b8160939
Radek Jajko <radek235s@gmail.com>
2020-12-07 16:03:12 +0100

    Add Vue Unit tests for Default Logging Type Comp
    Simple Unit tests to validate functionality of that component. #1447

---
+++ scadalts-ui/tests/unit/SystemSettings/LoggingTypeSettings.spec.js
@@ -0,0 +1,76 @@
+
+import i18n from '@/i18n'
+
+import Vuex from 'vuex'
+import { createLocalVue, mount } from '@vue/test-utils'
+import DefaultLoggingTypeSettingsComponent from '@/views/SystemSettings/DefaultLoggTypeComponent'
+import { expect } from 'chai'
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+const systemSettings = {
+    state: {
+        defaultLoggingType: 1
+    },
+
+    mutations: {
+        setDefaultLoggingType(state, defaultLoggingType) {
+            state.defaultLoggingType = defaultLoggingType;
+        }
+    },
+
+    actions: {
+        getDefaultLoggingType(context) {
+            return new Promise(resolve => {
+                context.state.defaultLoggingType = 3;
+                resolve(3)
+            })
+        },
+        saveDefaultLoggingType(context) {
+            return new Promise(resolve => {
+                resolve(true)
+            })
+        }
+    }
+}
+
+const modules = {
+    systemSettings
+}
+
+
+const store = new Vuex.Store({modules})
+
+
+describe('SystemSettings - LoggingType Settings Tests', () => {
+
+    const wrapper = mount(DefaultLoggingTypeSettingsComponent, {
+        store, localVue, i18n
+     });
+
+    it('Initialize Component', () => {
+        expect(wrapper.name()).to.equal('DefaultLoggingTypeSettingsComponent')
+    })
+
+    it('Test loading variable from Vuex store', () => {
+        expect(wrapper.vm.defaultLoggingType).to.equal(3);
+        expect(wrapper.vm.defaultLoggingTypeStore).to.equal(3);
+    })
+
+    it('Test i18n English Translation', () => {
+        expect(wrapper.findAll('option').at(1).html()).to.contain("All data")
+        expect(wrapper.findAll('option').at(2).html()).to.contain("Do not log")
+    })
+
+    it('Test value chagne', async () => {
+        wrapper.vm.defaultLoggingType = 5;
+        wrapper.vm.saveData();
+        wrapper.vm.$nextTick(() => {
+            expect(wrapper.vm.defaultLoggingType).to.equal(5);
+            expect(wrapper.vm.defaultLoggingTypeStore).to.equal(5);
+        })
+        
+    })
+
+})
