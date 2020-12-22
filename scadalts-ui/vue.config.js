a90932a244ed91b077532ae235fc688d5aca1672
Radek Jajko <radek235s@gmail.com>
2020-12-22 13:51:08 +0100

    Update SystemSettings UI design
    Change System Settings components from uiv library to Vuetify. #1475

--- scadalts-ui/vue.config.js
+++ scadalts-ui/vue.config.js
@@ -32,5 +32,13 @@ module.exports = {
   },
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  "devServer": {
+    "proxy": {
+      '^/api': {
+        target: 'http://localhost:8080/ScadaBR',
+        changeOrigin: true
+      },
+    }
+  }
 }
