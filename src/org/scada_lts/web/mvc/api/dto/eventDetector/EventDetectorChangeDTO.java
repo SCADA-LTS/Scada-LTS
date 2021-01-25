9a6c8ff96b47e1ad5aac83e37eab3854afff7131
Artur Wolak <arturwolak22@gmail.com>
2021-01-25 10:58:38 +0100

    EventDetectorChangeDTO changed

--- src/org/scada_lts/web/mvc/api/dto/eventDetector/EventDetectorChangeDTO.java
+++ src/org/scada_lts/web/mvc/api/dto/eventDetector/EventDetectorChangeDTO.java
@@ -4,55 +4,22 @@ import com.serotonin.mango.vo.DataPointVO;
 import com.serotonin.mango.vo.event.PointEventDetectorVO;
 
 public class EventDetectorChangeDTO extends EventDetectorDTO{
-    private int duration;
-    private int durationType;
-    private boolean binaryState;
 
     public EventDetectorChangeDTO() { }
 
-    public EventDetectorChangeDTO(String xid, String alias, int alarmLevel, int duration, int durationType, boolean binaryState) {
+    public EventDetectorChangeDTO(String xid, String alias, int alarmLevel) {
         super(xid, alias, alarmLevel);
-        this.duration = duration;
-        this.durationType = durationType;
-        this.binaryState = binaryState;
     }
 
     @Override
     public PointEventDetectorVO createPointEventDetectorVO(DataPointVO dataPointVO) {
         PointEventDetectorVO ped = new PointEventDetectorVO();
-        ped.setDetectorType(PointEventDetectorVO.TYPE_BINARY_STATE);
+        ped.setDetectorType(PointEventDetectorVO.TYPE_POINT_CHANGE);
         ped.njbSetDataPoint(dataPointVO);
         ped.setXid(getXid());
         ped.setAlias(getAlias());
         ped.setAlarmLevel(getAlarmLevel());
 
-        ped.setDuration(duration);
-        ped.setDurationType(durationType);
-        ped.setBinaryState(binaryState);
         return ped;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public int getDurationType() {
-        return durationType;
-    }
-
-    public void setDurationType(int durationType) {
-        this.durationType = durationType;
-    }
-
-    public boolean isBinaryState() {
-        return binaryState;
-    }
-
-    public void setBinaryState(boolean binaryState) {
-        this.binaryState = binaryState;
     }
 }
