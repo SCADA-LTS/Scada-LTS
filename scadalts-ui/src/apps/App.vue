694cd8780c5d4be1f4136d1c7528ae56175e845f
Radek Jajko <radek235s@gmail.com>
2021-02-10 12:08:26 +0100

    Fix add more general css specification. #1536

--- scadalts-ui/src/apps/App.vue
+++ scadalts-ui/src/apps/App.vue
@@ -119,7 +119,7 @@ td > select,
 td > textarea {
 	border-style: solid;
 }
-td > select, div[id*='Content'] select, div[id*='Content'] textarea {
+td > select, div[id*='Content'] select, div[id*='Content'] textarea, #viewContent select {
 	background-color: rgb(221, 221, 221);
 	border: 1px solid #39B54A;
 	appearance: auto;
