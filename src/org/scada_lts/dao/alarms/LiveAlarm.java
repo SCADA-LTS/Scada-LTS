6bd61b62312883782f0060b2e2550676c56c9a03
grzesiekb <grzegorz.bylica@gmail.com>
2021-03-12 13:01:21 +0100

    #1565 ADD comment for dataPointId

--- src/org/scada_lts/dao/alarms/LiveAlarm.java
+++ src/org/scada_lts/dao/alarms/LiveAlarm.java
@@ -43,6 +43,11 @@ public class LiveAlarm {
     private String inactivationTime;
     private String level;
     private String name;
+    /**
+    * Added pointId information so that additional information about the point can be get, e.g.:
+     * 1. Point desc,
+     * 2. Event text render
+    **/
     private int dataPointId;
 
     public int getId() {
