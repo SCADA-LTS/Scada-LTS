4b28b381810bfe2171c8cba38522d00ca3b958e2
Artur Wolak <arturwolak22@gmail.com>
2021-05-11 15:31:47 +0200

    refactor #1606

--- src/org/scada_lts/spring/LoginAuthenticationSuccessHandler.java
+++ src/org/scada_lts/spring/LoginAuthenticationSuccessHandler.java
@@ -1,7 +1,5 @@
 package org.scada_lts.spring;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
@@ -13,17 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-public class MySimpleUrlAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
-
-    protected Log logger = LogFactory.getLog(this.getClass());
-
-    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+public class LoginAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response, Authentication authentication)
             throws IOException {
 
-        redirectStrategy.sendRedirect(request, response, "/springLogin.htm");
+        RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+        redirectStrategy.sendRedirect(request, response, "/loginRedirect");
     }
 }
