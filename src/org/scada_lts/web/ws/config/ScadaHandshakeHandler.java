d95d8e468b107c9b638ae93f68d598f8c35bc0b4
Artur Wolak <arturwolak22@gmail.com>
2021-05-13 12:00:54 +0200

    webSockets refactor #1606

--- src/org/scada_lts/web/ws/config/ScadaHandshakeHandler.java
+++ src/org/scada_lts/web/ws/config/ScadaHandshakeHandler.java
@@ -10,41 +10,33 @@ import org.apache.commons.logging.LogFactory;
 import org.scada_lts.web.ws.beans.ScadaPrincipal;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
-import com.serotonin.mango.Common;
 import com.serotonin.mango.vo.User;
 
-public class ScadaHandshakeHandler   extends DefaultHandshakeHandler {
+public class ScadaHandshakeHandler extends DefaultHandshakeHandler {
     private final Log LOG = LogFactory.getLog(ScadaHandshakeHandler.class);
-    
-    
+
     @Override
     protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
-//        Principal principal = request.getPrincipal();
-//
-//        if (principal == null)
-//        {
-        ServletServerHttpRequest req = (ServletServerHttpRequest)request;
-        HttpSession session = req.getServletRequest().getSession();
-        final User user = (User) session.getAttribute("sessionUser");
-        //final User user = Common.getUser();
+        Principal principal = request.getPrincipal();
 
-        if( user != null ) {
-            LOG.debug("user.username: " + user.getUsername());
-            LOG.debug("user.id: " + user.getId());
+        if (principal == null || principal instanceof UsernamePasswordAuthenticationToken)
+        {
+            ServletServerHttpRequest req = (ServletServerHttpRequest)request;
+            HttpSession session = req.getServletRequest().getSession();
+            final User user = (User) session.getAttribute("sessionUser");
+
+            if(user != null) {
+                LOG.debug("user.username: " + user.getUsername());
+                LOG.debug("user.id: " + user.getId());
+            }
+
+            principal = new ScadaPrincipal(user);
         }
-
-////            principal=new Principal() {
-////                @Override
-////                public String getName() {
-////                    return user!=null?user.getUsername():"";
-////                }
-////            };
-        Principal principal = new ScadaPrincipal(user);
-//        }
-        LOG.debug("principal: " +principal.getName());
+        LOG.debug("principal: " + principal.getName());
         return principal;
     }
 }
