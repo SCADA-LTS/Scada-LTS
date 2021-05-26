110040828edc453f07a18c55c9a11985771a34c1
Radek Jajko <radek235s@gmail.com>
2021-05-26 13:36:14 +0200

    Refactor AMQP Code #966

---
+++ src/org/scada_lts/ds/amqp/DurabilityType.java
@@ -0,0 +1,5 @@
+package org.scada_lts.ds.amqp;
+
+public enum DurabilityType {
+    TRANSIENT,DURABLE
+}
---
+++ src/org/scada_lts/ds/amqp/MessageAckType.java
@@ -0,0 +1,5 @@
+package org.scada_lts.ds.amqp;
+
+public enum MessageAckType {
+    ACK, NO_ACK
+}
