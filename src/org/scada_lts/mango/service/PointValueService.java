30d35c4ae589eb2d710ba66bd510417ca53e1fc3
Limraj <35842300+Limraj@users.noreply.github.com>
2021-06-05 22:35:01 +0200

    #1726 Logging script execution error details - added logging error execute script, added LoggingScriptUtils
    (cherry picked from commit 0e528d8)

--- src/org/scada_lts/mango/service/PointValueService.java
+++ src/org/scada_lts/mango/service/PointValueService.java
@@ -34,6 +34,7 @@ import com.serotonin.mango.rt.dataImage.types.*;
 import com.serotonin.mango.rt.dataSource.meta.MetaDataSourceRT;
 import com.serotonin.mango.rt.dataSource.meta.MetaPointLocatorRT;
 import com.serotonin.mango.rt.dataSource.meta.ScriptExecutor;
+import com.serotonin.mango.util.LoggingScriptUtils;
 import com.serotonin.mango.vo.DataPointVO;
 import com.serotonin.mango.vo.dataSource.DataSourceVO;
 import com.serotonin.mango.vo.dataSource.meta.MetaDataSourceVO;
@@ -65,6 +66,8 @@ import com.serotonin.mango.vo.bean.LongPair;
 import com.serotonin.monitor.IntegerMonitor;
 import com.serotonin.util.queue.ObjectQueue;
 
+import static com.serotonin.mango.util.LoggingScriptUtils.loggingErrorExecutionScript;
+
 /**
  * Base on the PointValueDao
  *
@@ -79,6 +82,8 @@ public class PointValueService implements MangoPointValues, MangoPointValuesWith
     private static PointValueAdnnotationsDAO pointValueAnnotationsDAO = new PointValueAdnnotationsDAO();
     private DataPointService dataPointService = new DataPointService();
     private DataSourceService dataSourceService = new DataSourceService();
+
+    private static final Log LOG = LogFactory.getLog(PointValueService.class);
 
     public PointValueService() {
 
@@ -667,7 +672,7 @@ public class PointValueService implements MangoPointValues, MangoPointValuesWith
 
             dataPointService.save(value, dataPoint.getXid(), metaPointLocatorVO.getDataTypeId());
         } catch (Exception e) {
-            //
+            LoggingScriptUtils.loggingErrorExecutionScript(e, LOG, "dataPointXid: " + xid);
         }
     }
 
