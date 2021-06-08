0909c2dd0001037a8a6479d008554cb6483038d7
Limraj <35842300+Limraj@users.noreply.github.com>
2021-06-08 09:43:45 +0200

    #1726 Logging script execution error details - added logging ScriptComponent, correct msg, correct PointValueService.updateMetaDataPointByScript
    (cherry picked from commit 095582d)

--- src/com/serotonin/mango/web/dwr/ViewDwr.java
+++ src/com/serotonin/mango/web/dwr/ViewDwr.java
@@ -30,6 +30,7 @@ import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.serotonin.mango.util.LoggingScriptUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.directwebremoting.WebContext;
@@ -92,6 +93,7 @@ import com.serotonin.mango.web.dwr.beans.ViewComponentState;
 import com.serotonin.util.StringUtils;
 import com.serotonin.web.dwr.DwrResponseI18n;
 import com.serotonin.web.dwr.MethodFilter;
+import org.scada_lts.dao.model.ScadaObjectIdentifier;
 
 import static com.serotonin.mango.util.LoggingScriptUtils.infoErrorExecutionScript;
 
@@ -240,7 +242,7 @@ public class ViewDwr extends BaseDwr {
 			DataPointRT dataPointRT = null;
 			if (pointComponent.tgetDataPoint() != null)
 				dataPointRT = rtm.getDataPoint(pointComponent.tgetDataPoint().getId());
-
+			model.put(LoggingScriptUtils.VIEW_IDENTIFIER, new ScadaObjectIdentifier(view.getId(), view.getXid(), view.getName()));
 			ViewComponentState state = preparePointComponentState(pointComponent, user, dataPointRT, model, request);
 
 			if (!edit) {
