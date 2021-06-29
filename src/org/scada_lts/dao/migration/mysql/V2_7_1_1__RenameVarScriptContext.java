b7cc7145bf340ca30d708dc378e7a2690d496732
Artur Wolak <arturwolak22@gmail.com>
2021-06-29 15:50:49 +0200

    #1733 - xid as var name - migration update

--- src/org/scada_lts/dao/migration/mysql/V2_7_1_1__RenameVarScriptContext.java
+++ src/org/scada_lts/dao/migration/mysql/V2_7_1_1__RenameVarScriptContext.java
@@ -3,6 +3,7 @@ package org.scada_lts.dao.migration.mysql;
 import br.org.scadabr.vo.scripting.ContextualizedScriptVO;
 import com.serotonin.db.IntValuePair;
 import com.serotonin.mango.vo.DataPointVO;
+import com.serotonin.mango.vo.dataSource.meta.MetaPointLocatorVO;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
@@ -28,6 +29,7 @@ public class V2_7_1_1__RenameVarScriptContext extends BaseJavaMigration {
 
         try {
             migrateScripts(jdbcTmp);
+            migrateMetaDataPoints(jdbcTmp);
         } catch (Exception ex) {
             LOG.error(ex.getMessage(), ex);
             throw ex;
@@ -63,17 +65,42 @@ public class V2_7_1_1__RenameVarScriptContext extends BaseJavaMigration {
         }
     }
 
+    private void migrateMetaDataPoints(JdbcTemplate jdbcTmp) {
+        List<DataPointVO> datapoints = jdbcTmp.query("SELECT id, data FROM datapoints", (resultSet, i) -> {
+            try (InputStream inputStream = resultSet.getBinaryStream("data");
+                 ObjectInputStream objectInputStream = new ObjectInputStream(inputStream)) {
+                DataPointVO dataPointVO = (DataPointVO) objectInputStream.readObject();
+                dataPointVO.setId(resultSet.getInt("id"));
+                if (dataPointVO.getPointLocator() instanceof MetaPointLocatorVO)
+                    updateMetaDataPoint(dataPointVO.getPointLocator());
+
+                return dataPointVO;
+            } catch (IOException | ClassNotFoundException ex) {
+                LOG.error(ex.getMessage(), ex);
+                return null;
+            }
+        });
+
+        boolean isNull = datapoints.stream().anyMatch(Objects::isNull);
+        if (isNull) {
+            throw new IllegalStateException("ContextualizedScriptVO is null!");
+        }
+
+        for (DataPointVO datapoint : datapoints) {
+            jdbcTmp.update("UPDATE datapoints set data = ? WHERE id = ?",
+                    new SerializationData().writeObject(datapoint), datapoint.getId());
+        }
+    }
+
     private String updateScript(String script, List<IntValuePair> pointsOnContext) {
-        List<IntValuePair> sorted = new ArrayList<>(pointsOnContext);
-        sorted.sort(Comparator.comparing(IntValuePair::getKey));
-        Collections.reverse(sorted);
-        for (IntValuePair point : sorted) {
+        String updated = script;
+        for (IntValuePair point : pointsOnContext) {
             if (point.getValue().matches("^p[0-9]+$")) {
                 String xid = new DataPointDAO().getDataPoint(point.getKey()).getXid();
-                script = script.replaceAll(point.getValue(), xid.toLowerCase().trim());
+                updated = updated.replaceAll("(?<![a-zA-Z0-9])" + point.getValue() + "(?![a-zA-Z0-9])", xid.toLowerCase().trim());
             }
         }
-        return script;
+        return updated;
     }
 
     private void updatePointsOnContext(List<IntValuePair> pointsOnContext) {
@@ -83,6 +110,11 @@ public class V2_7_1_1__RenameVarScriptContext extends BaseJavaMigration {
                 point.setValue(dataPointVO.getXid().toLowerCase().trim());
             }
         }
+    }
+
+    private void updateMetaDataPoint(MetaPointLocatorVO locator) {
+        locator.setScript(updateScript(locator.getScript(), locator.getContext()));
+        updatePointsOnContext(locator.getContext());
     }
 
 }
