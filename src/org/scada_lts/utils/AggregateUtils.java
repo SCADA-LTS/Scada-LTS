5f9c4eb3bece0566e44b3217a016c3ddea2c4b21
Artur Wolak <arturwolak22@gmail.com>
2021-07-09 12:51:51 +0200

    #1772 - API for aggregated point values for charts - different datapoint types

--- src/org/scada_lts/utils/AggregateUtils.java
+++ src/org/scada_lts/utils/AggregateUtils.java
@@ -8,8 +8,9 @@ import java.io.IOException;
 public final class AggregateUtils {
 
     private static final String API_AMCHARTS_AGGREGATION_ENABLED_KEY = "api.amcharts.aggregation.enabled";
-    private static final String API_AMCHARTS_AGGREGATION_VALUES_MAX_KEY = "api.amcharts.aggregation.valuesMax";
-    private static final int NUMBER_OF_VALUES_MAX_DEFAULT = 10000;
+    private static final String API_AMCHARTS_AGGREGATION_VALUES_LIMIT_KEY = "api.amcharts.aggregation.valuesLimit";
+    private static final String API_AMCHARTS_AGGREGATION_LIMIT_MULTIPLIER_KEY = "api.amcharts.aggregation.limitMultiplier";
+    private static final int NUMBER_OF_VALUES_LIMIT_DEFAULT = 10000;
     private static final org.apache.commons.logging.Log LOG = LogFactory.getLog(AggregateUtils.class);
 
 
@@ -31,16 +32,25 @@ public final class AggregateUtils {
     }
 
     private static long calculate(long startTs, long endTs, int numberOfPoints) {
-        long result = Math.round((endTs - startTs)/(getNumberOfValuesMax() *1.5/numberOfPoints));
+        long result = Math.round((endTs - startTs)/(getNumberOfValuesLimit() *1.5/numberOfPoints));
         return result == 0 ? 1 : result;
     }
 
-    private static int getNumberOfValuesMax() {
+    public static int getNumberOfValuesLimit() {
         try {
-            return ScadaConfig.getInstance().getInt(API_AMCHARTS_AGGREGATION_VALUES_MAX_KEY, NUMBER_OF_VALUES_MAX_DEFAULT);
+            return ScadaConfig.getInstance().getInt(API_AMCHARTS_AGGREGATION_VALUES_LIMIT_KEY, NUMBER_OF_VALUES_LIMIT_DEFAULT);
         } catch (IOException e) {
             LOG.error(e.getMessage());
             return 10000;
+        }
+    }
+
+    public static double getApiAmchartsAggregationLimitMultiplierKey() {
+        try {
+            return Double.parseDouble(ScadaConfig.getInstance().getProperty(API_AMCHARTS_AGGREGATION_LIMIT_MULTIPLIER_KEY));
+        } catch (IOException e) {
+            LOG.error(e.getMessage());
+            return 1;
         }
     }
 }
