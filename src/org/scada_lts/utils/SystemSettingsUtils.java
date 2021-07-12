cbdf83dd7e95dabda7b15a8ac924f3031ffda042
kamiljarmusik <kamil.jarmusik@gmail.com>
2021-07-12 20:51:21 +0200

    #1788 Added configuration in runtime for aggregation - correct valuesLimit, getBooleanValueOrDefault

--- src/org/scada_lts/utils/SystemSettingsUtils.java
+++ src/org/scada_lts/utils/SystemSettingsUtils.java
@@ -5,7 +5,7 @@ import org.scada_lts.web.mvc.api.AggregateSettings;
 public class SystemSettingsUtils {
 
     public static String validateAggregateSettings(AggregateSettings body) {
-        String msg = ValidationUtils.msgIfNullOrInvalid("valuesLimit must be >= 0; ", body.getValuesLimit(), a -> a < 0);
+        String msg = ValidationUtils.msgIfNullOrInvalid("valuesLimit must be >= 0; ", body.getValuesLimit(), a -> a <= 0);
         msg += ValidationUtils.msgIfNullOrInvalid("limitFactor must be > 0; ", body.getLimitFactor(), a -> a <= 0.0);
         msg += ValidationUtils.msgIfNull("Correct property enabled; ", body.isEnabled());
         return msg;
