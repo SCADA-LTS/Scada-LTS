3b1eb58721bb8f7958915fb49703265f2cd4e8de
Radek Jajko <radek235s@gmail.com>
2021-08-23 11:35:39 +0200

    #1823 Fix FrontendUnit tests

--- scadalts-ui/tests/unit/WatchListJsonChart.spec.js
+++ scadalts-ui/tests/unit/WatchListJsonChart.spec.js
@@ -30,17 +30,6 @@ describe('WatchListJsonChart.vue Test', () => {
 
 	it('Initialize blank JsonChart', () => {
 		expect(wrapper.name()).to.equal('WatchListJsonChart');
-		expect(wrapper.vm.pointId).to.equal('1');
-		expect(wrapper.vm.chartType).to.equal('live');
-	});
-
-	it('Change to Static chart', () => {
-		wrapper.find('#static-btn-1').trigger('click');
-		expect(wrapper.vm.chartType).to.equal('static');
-	});
-
-	it('Change to Compare chart', () => {
-		wrapper.find('#compare-btn-1').trigger('click');
-		expect(wrapper.vm.chartType).to.equal('compare');
+		
 	});
 });
