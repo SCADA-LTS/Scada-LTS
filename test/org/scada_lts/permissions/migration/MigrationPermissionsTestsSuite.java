2b397b344248b0ae926d7f029eefe53c2ff15ef0
Kamil Jarmusik <hsfgdag8z>
2021-10-11 16:29:49 +0200

    key is determined only on the basis of permissions> 0

--- test/org/scada_lts/permissions/migration/MigrationPermissionsTestsSuite.java
+++ test/org/scada_lts/permissions/migration/MigrationPermissionsTestsSuite.java
@@ -8,7 +8,8 @@ import org.junit.runners.Suite;
         DataPointMigrationPermissionsCommandTest.class,
         DataSourceMigrationPermissionsCommandTest.class,
         MigrationPermissionsUtilsTest.class,
-        GenerateDataPointPermissionTest.class
+        GenerateDataPointPermissionTest.class,
+        AccessesTest.class
 })
 public class MigrationPermissionsTestsSuite {
 }
