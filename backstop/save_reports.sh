ad96feed3044896a6325727676a0d32dd4494699
Kamil Jarmusik <hsfgdag8z>
2021-10-19 10:48:03 +0200

    correct save_reports.sh

--- backstop/save_reports.sh
+++ backstop/save_reports.sh
@@ -1,6 +1,9 @@
-cd backstop_data
-mkdir $1
-cp -R ./html_report ./$1
-cp -R ./bitmaps_reference ./$1
-cp -R ./bitmaps_test ./$1
+cd backstop_data;
+mkdir $1;
+cp -arf html_report $1;
+cp -arf bitmaps_reference $1;
+cp -arf bitmaps_test $1;
+rm -rf html_report;
+rm -rf bitmaps_reference;
+rm -rf bitmaps_test;
 cd ..
