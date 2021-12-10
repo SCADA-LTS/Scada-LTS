f7d7bc67c9f9c5557ff3cfcf64643836b596ae5e
kamiljarmusik <kamil.jarmusik@gmail.com>
2021-12-10 09:53:50 +0100

    #1606 Integrate REST API with permissions - refator spring-security.xml; removal unused class; configure WebSocketConfig.java

--- src/org/scada_lts/web/ws/auth/WebSocketConfig.java
+++ src/org/scada_lts/web/ws/auth/WebSocketConfig.java
@@ -1,15 +1,16 @@
 package org.scada_lts.web.ws.auth;
 
-import org.scada_lts.web.ws.config.ScadaHandshakeHandler;
+import org.scada_lts.web.ws.config.LoggingChannelInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.ExpiringSession;
 import org.springframework.session.web.socket.config.annotation.AbstractSessionWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.server.HandshakeHandler;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableScheduling
@@ -18,9 +19,28 @@ public class WebSocketConfig<S extends ExpiringSession> extends AbstractSessionW
 
     @Override
     protected void configureStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-scada").withSockJS();
-        registry.addEndpoint("/ws-scada/event").withSockJS();
-        registry.addEndpoint("/ws-scada/alarmLevel").withSockJS();
+        registry.addEndpoint("/ws-scada")
+                .setAllowedOrigins("http://localhost:3000")
+                .withSockJS();
+        registry.addEndpoint("/ws-scada/event")
+                .setAllowedOrigins("http://localhost:3000")
+                .withSockJS()
+                .setStreamBytesLimit(512 * 1024)
+                .setHttpMessageCacheSize(1000)
+                .setDisconnectDelay(30 * 1000);
+        registry.addEndpoint("/ws-scada/alarmLevel")
+                .setAllowedOrigins("http://localhost:3000")
+                .withSockJS()
+                .setStreamBytesLimit(512 * 1024)
+                .setHttpMessageCacheSize(1000)
+                .setDisconnectDelay(30 * 1000);
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.setSendTimeLimit(15 * 1000)
+                .setSendBufferSizeLimit(512 * 1024)
+                .setMessageSizeLimit(128 * 1024);
     }
 
     @Override
@@ -28,31 +48,20 @@ public class WebSocketConfig<S extends ExpiringSession> extends AbstractSessionW
         registry.enableSimpleBroker("/queue/", "/topic/");
         registry.setApplicationDestinationPrefixes("/app");
     }
-/*
+
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(authChannelInterceptorAdapter(webSocketAuthenticatorService()));
-        super.configureClientInboundChannel(registration);
-    }*/
-
-    @Bean
-    public HandshakeHandler handshakeHandler() {
-        return new ScadaHandshakeHandler();
+        registration.interceptors(loggingChannelInterceptor());
     }
-/*
+
+    @Override
+    public void configureClientOutboundChannel(ChannelRegistration registration) {
+        registration.taskExecutor().corePoolSize(8);
+        registration.interceptors(loggingChannelInterceptor());
+    }
+
     @Bean
     public LoggingChannelInterceptor loggingChannelInterceptor() {
         return new LoggingChannelInterceptor();
     }
-
-    @Bean
-    public WebSocketAuthenticatorService webSocketAuthenticatorService() {
-        return new WebSocketAuthenticatorService();
-    }
-
-    @Bean
-    public ChannelInterceptor authChannelInterceptorAdapter(WebSocketAuthenticatorService webSocketAuthenticatorService) {
-        return new AuthChannelInterceptorAdapter(webSocketAuthenticatorService);
-    }*/
-
 }
