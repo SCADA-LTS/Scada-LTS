c8c8a313e487d0d8a0774c35d2a203aa2fb5cef3
kamiljarmusik <kamil.jarmusik@gmail.com>
2021-12-15 10:25:37 +0100

    #1606 Integrate REST API with permissions - correct WebSocketAuthorizationSecurityConfig, spring-security.xml

--- src/org/scada_lts/web/ws/auth/WebSocketAuthorizationSecurityConfig.java
+++ src/org/scada_lts/web/ws/auth/WebSocketAuthorizationSecurityConfig.java
@@ -12,7 +12,6 @@ public class WebSocketAuthorizationSecurityConfig extends AbstractSecurityWebSoc
         messages.anyMessage().authenticated();
     }
 
-    // TODO: For test purpose (and simplicity) i disabled CSRF, but you should re-enable this and provide a CRSF endpoint.
     @Override
     protected boolean sameOriginDisabled() {
         return true;
