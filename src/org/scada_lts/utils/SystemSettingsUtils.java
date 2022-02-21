2ac37ea694e1d1275453fca638291b7046973962
Kamil Jarmusik <hsfgdag8z>
2022-02-21 22:52:58 +0100

    #2101 Avoided duplicate values - added datapoint.runtime.synchronized

--- src/org/scada_lts/utils/SystemSettingsUtils.java
+++ src/org/scada_lts/utils/SystemSettingsUtils.java
@@ -1,8 +1,22 @@
 package org.scada_lts.utils;
 
+import org.apache.commons.logging.LogFactory;
+import org.scada_lts.config.ScadaConfig;
 import org.scada_lts.web.mvc.api.AggregateSettings;
 
 public class SystemSettingsUtils {
+
+    private static final String DATAPOINT_RUNTIME_SYNCHRONIZED_KEY = "datapoint.runtime.synchronized";
+    private static final org.apache.commons.logging.Log LOG = LogFactory.getLog(SystemSettingsUtils.class);
+
+    public static boolean isDataPointRtSynchronized() {
+        try {
+            return ScadaConfig.getInstance().getBoolean(DATAPOINT_RUNTIME_SYNCHRONIZED_KEY, false);
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+            return false;
+        }
+    }
 
     public static String validateAggregateSettings(AggregateSettings body) {
         String msg = ValidationUtils.msgIfNullOrInvalid("valuesLimit must be >= 0; ", body.getValuesLimit(), a -> a <= 0);
