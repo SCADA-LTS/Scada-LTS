ae89b8d7fd9a80163b903e27c8554f12036df2ca
kamiljarmusik <kamil.jarmusik@gmail.com>
2022-05-19 20:40:33 +0200

    #2215 Fixed graphic component visibility after refreshing page

--- scadalts-ui/src/views/GraphicalViews/GraphicalViewPage.vue
+++ scadalts-ui/src/views/GraphicalViews/GraphicalViewPage.vue
@@ -243,7 +243,7 @@
 		>
 			<component
 				v-for="(cmp, index) in viewComponents"
-				:key="index"
+				:key="key(cmp)"
 				:is="cmp.defName.toUpperCase()"
 				:component="cmp"
 				@update="updateComponents"
@@ -268,6 +268,8 @@ export default {
 		return {
 			changes: 0,
 			hiddenComponents: [],
+			viewComponents: [],
+			graphicViewId: 0
 		};
 	},
 
@@ -283,11 +285,6 @@ export default {
 	computed: {
 		viewPage() {
 			return this.$store.state.graphicalViewModule.graphicalPage;
-		},
-		viewComponents() {
-			if (!!this.$store.state.graphicalViewModule.graphicalPage) {
-				return this.$store.getters.viewComponentsGetter;
-			}
 		},
 		viewSize() {
 			return this.$store.state.graphicalViewModule.resolution;
@@ -320,6 +317,8 @@ export default {
 					if (this.editMode && this.userAccess < 2) {
 						this.$store.commit('SET_GRAPHICAL_PAGE_EDIT', false);
 					}
+					this.graphicViewId = graphicalViewId;
+					this.viewComponents = this.$store.getters.viewComponentsGetter;
 				} catch (e) {
 					console.error(e);
 					if (e.status === 401) {
@@ -383,6 +382,9 @@ export default {
 		deleteComponent() {
 			this.activeComponent.deleteComponent();
 		},
+        key(cmp) {
+            return this.graphicViewId + "_" + cmp.index;
+        },
 	},
 
 	watch: {
