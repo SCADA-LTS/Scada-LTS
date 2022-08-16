c29db6ac8f40dcdb0536c94c3552afbd65f77a77
kamiljarmusik <kamil.jarmusik@gmail.com>
2022-08-16 13:46:15 +0200

    #2314 Fixed bug after shutdown tomcat without logout -

---
+++ src/org/scada_lts/session/SetDataSessionFilter.java
@@ -0,0 +1,31 @@
+package org.scada_lts.session;
+
+import com.serotonin.mango.Common;
+import com.serotonin.mango.vo.User;
+import org.scada_lts.mango.service.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class SetDataSessionFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if(request instanceof HttpServletRequest) {
+            HttpServletRequest req = (HttpServletRequest) request;
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication != null && authentication.isAuthenticated()) {
+                User user = Common.getUser(req);
+                if (user == null) {
+                    UserService userService = new UserService();
+                    Common.setUser(req, userService.getUser(authentication.getName()));
+                }
+            }
+        }
+        chain.doFilter(request, response);
+    }
+
+}
