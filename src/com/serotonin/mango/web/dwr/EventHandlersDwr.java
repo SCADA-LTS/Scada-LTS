c44ce21fc177a412e4142a4e706ea6d8c1a8a7e4
Kamil Jarmusik <kamil.jarmusik@gmail.com>
2023-01-17 22:03:01 +0100

    #1143 Fixed the handler working after it was created - corrected EventHandlersDwr

--- src/com/serotonin/mango/web/dwr/EventHandlersDwr.java
+++ src/com/serotonin/mango/web/dwr/EventHandlersDwr.java
@@ -25,8 +25,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 
-import com.serotonin.mango.rt.dataImage.DataPointRT;
-import com.serotonin.mango.rt.event.type.EventType;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.directwebremoting.WebContextFactory;
