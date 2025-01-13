80c735c7cd0113c592f189c2e325ea33ba030d7d
Patrykb0802 <patrykbartyzel0@gmail.com>
2025-01-13 18:03:24 +0100

    #2693 Expansion of the Event search function
    Expanded search function for events

--- src/org/scada_lts/dao/event/EventDAO.java
+++ src/org/scada_lts/dao/event/EventDAO.java
@@ -1592,4 +1592,109 @@ public class EventDAO implements GenericDaoCR<EventInstance> {
 		int updates = DAO.getInstance().getJdbcTemp().update(EVENT_UNASSIGN_EVENT, new Object[]  { eventId } );
 		return updates > 0;
 	}
+
+	public List<EventInstance> searchNew(
+			String[] eventSourceTypes,
+			String[] statuses,
+			String[] alarmLevels,
+			Date startDate,
+			Date endDate,
+			String[] keywordArr,
+			int userId,
+			ResourceBundle bundle
+	) {
+		List<String> where = new ArrayList<>();
+		List<Object> params = new ArrayList<>();
+
+		StringBuilder sql = new StringBuilder();
+		sql.append(EVENT_SELECT_WITH_USER_DATA);
+		sql.append(" WHERE ue.userId=?");
+		params.add(userId);
+
+		if (eventSourceTypes != null && eventSourceTypes.length > 0) {
+			String inClause = buildInClause("e.typeId", eventSourceTypes.length);
+			where.add(inClause);
+			for (String src : eventSourceTypes) {
+				params.add(Integer.valueOf(src));
+			}
+		}
+
+		if (statuses != null && statuses.length > 0) {
+			List<String> orClauses = new ArrayList<>();
+			for (String s : statuses) {
+				if (EventsDwr.STATUS_ACTIVE.equals(s)) {
+					orClauses.add("( e.rtnApplicable='Y' AND e.rtnTs=0 )");
+				} else if (EventsDwr.STATUS_RTN.equals(s)) {
+					orClauses.add("( e.rtnApplicable='Y' AND e.rtnTs>0 )");
+				} else if (EventsDwr.STATUS_NORTN.equals(s)) {
+					orClauses.add("( e.rtnApplicable='N' )");
+				} else if (EventsDwr.STATUS_ASSIGNEE.equals(s)) {
+					orClauses.add("( " + STATUS_ASSIGNEE_CONDITION_SQL + " )");
+				}
+			}
+			if (!orClauses.isEmpty()) {
+				String statusBlock = "(" + String.join(" OR ", orClauses) + ")";
+				where.add(statusBlock);
+			}
+		}
+
+		if (alarmLevels != null && alarmLevels.length > 0) {
+			String inClause = buildInClause("e.alarmLevel", alarmLevels.length);
+			where.add(inClause);
+			for (String lvl : alarmLevels) {
+				params.add(Integer.valueOf(lvl));
+			}
+		}
+
+		if (startDate != null) {
+			where.add("e.activeTs >= ?");
+			params.add(startDate.getTime());
+		}
+		if (endDate != null) {
+			where.add("e.activeTs <= ?");
+			params.add(endDate.getTime());
+		}
+
+		if (keywordArr != null && keywordArr.length > 0) {
+			List<String> orKeywords = new ArrayList<>();
+			for (String kw : keywordArr) {
+				orKeywords.add(" e.message LIKE ? ");
+				params.add("%" + kw + "%");
+			}
+			if (!orKeywords.isEmpty()) {
+				where.add("(" + String.join(" OR ", orKeywords) + ")");
+			}
+		}
+
+		for (String s : where) {
+			sql.append(" AND ");
+			sql.append(s);
+		}
+
+		sql.append(" ORDER BY e.activeTs DESC");
+
+		final List<EventInstance> results = new ArrayList<>();
+		final UserEventRowMapper rowMapper = new UserEventRowMapper();
+		DAO.getInstance().getJdbcTemp().query(sql.toString(), params.toArray(), rs -> {
+			IUserCommentDAO userCommentDAO = ApplicationBeans.getUserCommentDaoBean();
+			while (rs.next()) {
+				EventInstance e = rowMapper.mapRow(rs, 0);
+				e.setEventComments(userCommentDAO.getEventComments(e));
+				results.add(e);
+			}
+			return null;
+		});
+
+		return results;
+	}
+
+	private String buildInClause(String columnName, int length) {
+		StringBuilder sb = new StringBuilder(columnName).append(" IN (");
+		for (int i = 0; i < length; i++) {
+			if (i > 0) sb.append(", ");
+			sb.append("?");
+		}
+		sb.append(")");
+		return sb.toString();
+	}
 }
