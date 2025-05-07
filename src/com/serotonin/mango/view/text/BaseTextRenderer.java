5150b7cda581321535cddbddb9096781e0f13e1f
patrykb0802 <patrykbartyzel0@gmail.com>
2025-05-07 09:45:59 +0200

    #2497 Add display format as Hex numbers or binary numbers

--- src/com/serotonin/mango/view/text/BaseTextRenderer.java
+++ src/com/serotonin/mango/view/text/BaseTextRenderer.java
@@ -45,13 +45,13 @@ abstract public class BaseTextRenderer implements TextRenderer, JsonSerializable
             List<ImplDefinition> d = new ArrayList<ImplDefinition>();
             d.add(AnalogRenderer.getDefinition());
             d.add(BinaryTextRenderer.getDefinition());
-            d.add(BinaryRenderer.getDefinition());
-            d.add(HexadecimalRenderer.getDefinition());
             d.add(MultistateRenderer.getDefinition());
             d.add(NoneRenderer.getDefinition());
             d.add(PlainRenderer.getDefinition());
             d.add(RangeRenderer.getDefinition());
             d.add(TimeRenderer.getDefinition());
+            d.add(BinaryRenderer.getDefinition());
+            d.add(HexadecimalRenderer.getDefinition());
             definitions = d;
         }
     }
