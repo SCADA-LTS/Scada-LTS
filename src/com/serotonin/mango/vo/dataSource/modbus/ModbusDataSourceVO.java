9de9a629190e962a82198f69ba72b828aa9f5aed
Limraj <kamil.jarmusik@gmail.com>
2025-07-16 22:57:40 +0200

    #3169 Corrected v2.8.0: - Corrected Modbus validation, increased timeout range to 180 000 ms

--- src/com/serotonin/mango/vo/dataSource/modbus/ModbusDataSourceVO.java
+++ src/com/serotonin/mango/vo/dataSource/modbus/ModbusDataSourceVO.java
@@ -197,8 +197,8 @@ abstract public class ModbusDataSourceVO<T extends ModbusDataSourceVO<?>>
 					"validate.greaterThanZero");
 		if (timeout <= 0)
 			response.addContextualMessage("timeout", "validate.greaterThanZero");
-		if (timeout > 30000)
-			response.addContextualMessage("timeout", "validate.0toArg", 30000);
+		if (timeout > 180000)
+			response.addContextualMessage("timeout", "validate.0toArg", 180000);
 		if (retries < 0)
 			response.addContextualMessage("retries",
 					"validate.cannotBeNegative");
