4a578783b7145d4d4cfbc9bf7550ca7cef024fa5
Kamil Jarmusik <kamil.jarmusik@gmail.com>
2025-10-28 11:11:49 +0100

    Revert "#3206 Fixed escaping fields in Event handlers, Users profiles, watch lists"
    This reverts commit 2b2db635f8cfae8f568b960476219fa0e17b81b0.

--- src/org/scada_lts/web/dwr/security/XssLocalizableMessageConverter.java
+++ src/org/scada_lts/web/dwr/security/XssLocalizableMessageConverter.java
@@ -2,29 +2,27 @@ package org.scada_lts.web.dwr.security;
 
 import com.serotonin.web.i18n.I18NUtils;
 import com.serotonin.web.i18n.LocalizableMessage;
-import org.directwebremoting.WebContext;
 import org.directwebremoting.WebContextFactory;
 import org.directwebremoting.convert.StringConverter;
-import org.directwebremoting.extend.*;
+import org.directwebremoting.extend.MarshallException;
+import org.directwebremoting.extend.OutboundContext;
+import org.directwebremoting.extend.OutboundVariable;
+import org.scada_lts.web.security.XssProtectUtils;
 
-import static org.scada_lts.web.dwr.security.utils.XssBeanConverterUtils.convertObjectEscaped;
-import static org.scada_lts.web.dwr.security.utils.XssBeanConverterUtils.convertObjectUnescaped;
-
-public class XssLocalizableMessageConverter  extends StringConverter {
-    public XssLocalizableMessageConverter() {
-    }
-
-    public OutboundVariable convertOutbound(Object data, OutboundContext outctx) throws MarshallException {
-        WebContext webctx = WebContextFactory.get();
-        LocalizableMessage lm = (LocalizableMessage)data;
-        convertObjectEscaped(data);
-        String s = lm.getLocalizedMessage(I18NUtils.getBundle(webctx.getHttpServletRequest()));
-        return super.convertOutbound(s, outctx);
-    }
+public class XssLocalizableMessageConverter extends StringConverter {
 
     @Override
-    public Object convertInbound(Class paramType, InboundVariable iv, InboundContext inctx) throws MarshallException {
-        convertObjectUnescaped(iv.getValue());
-        return super.convertInbound(paramType, iv, inctx);
+    public OutboundVariable convertOutbound(Object data, OutboundContext outctx) throws MarshallException {
+        if (data == null) {
+            return super.convertOutbound("", outctx);
+        }
+        LocalizableMessage lm = (LocalizableMessage) data;
+
+        String localized = lm.getLocalizedMessage(
+                I18NUtils.getBundle(WebContextFactory.get().getHttpServletRequest())
+        );
+
+        String escaped = XssProtectUtils.escapeHtml(localized);
+        return super.convertOutbound(escaped, outctx);
     }
 }
