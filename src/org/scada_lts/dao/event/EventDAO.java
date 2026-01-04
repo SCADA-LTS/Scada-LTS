559906ba22d9b0ccaa7e74fe7478715b22654380
kamiljarmusik <kamil.jarmusik@gmail.com>
2026-01-04 02:44:37 +0100

    #3237 Fixed Event Handlers: - Corrected EventDAO.getEventHandlers

--- src/org/scada_lts/dao/event/EventDAO.java
+++ src/org/scada_lts/dao/event/EventDAO.java
@@ -1379,7 +1379,7 @@ public class EventDAO implements GenericDaoCR<EventInstance> {
 	}
 	
 	public List<EventHandlerVO> getEventHandlers(int typeId, int ref1, int ref2) {
-		if(ref2 > 0 && (typeId != EventType.EventSources.SYSTEM && typeId != EventType.EventSources.AUDIT))
+		if(ref2 > 0 && (typeId == EventType.EventSources.DATA_POINT || typeId == EventType.EventSources.DATA_SOURCE || typeId == EventType.EventSources.PUBLISHER))
 			return DAO.getInstance().getJdbcTemp().query(EVENT_HANDLER_SELECT+" where "+ EVENT_HANDLER_FILTER_REF2, new Object[] {typeId, ref1, ref2}, new EventHandlerRowMapper());
 		return DAO.getInstance().getJdbcTemp().query(EVENT_HANDLER_SELECT+" where "+ EVENT_HANDLER_FILTER_N, new Object[] {typeId, ref1}, new EventHandlerRowMapper());
 	}
