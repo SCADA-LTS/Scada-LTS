11f911c934d4c43e920df267144748759cb308a5
Limraj <kamil.jarmusik@gmail.com>
2026-01-22 17:33:01 +0100

    #2779 Very slow editing of graphical chart: - Reduce limit to 200;

--- WebContent/resources/common.js
+++ WebContent/resources/common.js
@@ -1909,7 +1909,7 @@ class DataPointsSelect {
     constructor (dataPointsSelectDef) {
 
         this.excludePointsArray = dataPointsSelectDef.excludePointsArray || [];
-        this.limit = dataPointsSelectDef.limit || 500;
+        this.limit = dataPointsSelectDef.limit || 200;
         this.selectHtmlId = dataPointsSelectDef.selectHtmlId;
         this.inputHtmlId = dataPointsSelectDef.selectHtmlId + "_chosen .chosen-search input";
         this.listHtmlId = dataPointsSelectDef.selectHtmlId + "_chosen .chosen-results";
